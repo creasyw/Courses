ff0773dbcc1a445b5779ea9d19598de57d20cd1c
creasyw <creasywuqiong@gmail.com>
2011-09-15 12:48:45 -0500

    To Q09

---
+++ javacode/Networking/ReadServerFileTest.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class ReadServerFileTest
+{
+	public static void main (String args[])
+	{
+		ReadServerFile application = new ReadServerFile();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
