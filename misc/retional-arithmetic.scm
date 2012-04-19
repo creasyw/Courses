e968988c7ae662fc1e3bae93b26ea83e35e26e4e
creasyw <creasywuqiong@gmail.com>
2012-04-19 20:40:58 +0800

    rational arithmetic

---
+++ misc/retional-arithmetic.scm
@@ -0,0 +1,33 @@
+(define (gcd a b)
+  (if (= b 0) a
+    (gcd b (remainder a b))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (make-rat n d)
+  (let (( g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y)) (* (numer y) (denom x)))
+	    (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y)) (* (numer y) (denom x)))
+	     ((* (denom x) (denom y)))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y)) (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y)) (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y)) (* (denom x) (numer y))))
+
