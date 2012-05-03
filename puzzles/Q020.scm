0ad136746c1dcfb16c5b980c7ad5290cf594a5b9
creasyw <creasywuqiong@gmail.com>
2012-05-03 22:20:20 +0800

    Q20

--- puzzles/Q020.scm
+++ puzzles/Q020.scm
@@ -3,11 +3,11 @@
     (factorial (- base 1) (* product base))))
 
 (define (sum-digit input)
-  (define digit-list (string->list (number->string input)))
   (define (accu-digit result left)
     (if (null? left) result
       (accu-digit (+ result (char->digit (car left))) (cdr left))))
-  (accu-digit 0 digit-list))
+  (let ((digit-list (string->list (number->string input))))
+    (accu-digit 0 digit-list)))
 
 (define (sum-digit-factorial base)
   (sum-digit (factorial base 1)))
