39c7ebe8e0f3e76fd630b7323292bec4bc385904
creasyw <creasywuqiong@gmail.com>
2012-07-12 14:02:18 -0400

    estimate pi via monte-carlo

---
+++ misc/estimate-pi.scm
@@ -0,0 +1,15 @@
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+(define (cesaro-test)
+  (= (gcd (random 9999) (random 9999)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0) (/ trials-passed trials))
+	  ((experiment) (iter (- trials-remaining 1) (+ trials-passed 1)))
+	  (else
+	    (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+
