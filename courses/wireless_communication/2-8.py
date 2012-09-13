60914facd137a7538a5f34326d3b65276726a0a3
creasyw <creasywuqiong@gmail.com>
2012-09-13 15:03:17 -0500

    add several lines plotting

--- courses/wireless_communication/2-8.py
+++ courses/wireless_communication/2-8.py
@@ -10,7 +10,7 @@ def log(num):
 
 def main():
     ht = 50
-    hr = 15
+    hr = 2
     f = 900*10**6
     c = 3*10**8
     gr = [1, 0.316, 0.1, 0.01]
@@ -23,13 +23,19 @@ def main():
     reflect = (distance**2+(ht+hr)**2)**0.5
     los = (distance**2+(ht-hr)**2)**0.5
     phi = 2*pi*(reflect-los)/lambd
-    dright = distance[int(dc)-1:]
+    flat = distance[:ht]
+    decline = distance[ht:(dc+1)]
+    steep = distance[dc:]
 
     for i in range(len(gr)):
-        temp = gl/los+r*gr[i]*np.exp(phi*-1J)/reflect
+        temp = gl**0.5/los+r*(gr[i]**0.5)*np.exp(phi*-1J)/reflect
         pr = (lambd/4/pi)**2*(abs(temp)**2)
         plt.subplot(220+i+1)
-        plt.plot(10*log(distance),10*log(pr),'b', 10*log(dright),-20*log(dright),'g', 10*log(dright),-40*log(dright),'r')
+        plt.plot(10*log(distance),10*log(pr)-10*log10(pr[0]),'b', \
+                10*log(flat), np.zeros(len(flat)), 'y', \
+                10*log(decline),-20*log(decline),'g', 10*log(steep),-40*log(steep),'r')
+        plt.axvline(x=10*log10(ht), linestyle='-.')
+        plt.axvline(x=10*log10(dc), linestyle='-.')
         plt.title("Gr = %s"%gr[i])
 
     plt.show()
