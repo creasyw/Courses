36ab77fbca9a7c9a58ef8874a5fed32f1ff568c9
creasyw <creasywuqiong@gmail.com>
2012-09-20 15:08:08 -0500

    signal construction from multi base station

---
+++ courses/wireless_communication/3-10.py
@@ -0,0 +1,21 @@
+from scipy.stats.distributions import norm
+from math import exp, pi
+
+def q_func(x):
+    return 1-norm.cdf(x)
+
+def core_function(y):
+    return (q_func((5+8*(2**0.5)*y)/8/(2**0.5)))**2*exp(-y**2/2)
+
+def main():
+    lower = -100
+    upper = 100
+    interval = 0.01
+    steps = int((upper-lower)/interval)
+    result = 0
+    for i in range(steps):
+        result += core_function(lower+i*interval)*interval
+    print result/((2*pi)**0.5)
+
+if __name__ == "__main__":
+    main()
