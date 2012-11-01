d08ba81c92ba541b65018829d67891f84e122018
creasyw <creasywuqiong@gmail.com>
2012-11-01 02:27:31 -0500

    4-7

---
+++ courses/wireless_communication/4-7.py
@@ -0,0 +1,50 @@
+from math import exp, log, e
+from scipy.special import exp1
+
+def function(x):
+    return 1/x*exp(-x/10)-exp1(x/10)/10
+
+def question_a():
+    gamma = 0.0001
+    diff_min = abs(1 - function(gamma))
+    result = 0
+    while gamma<10:
+        diff = abs(1 - function(gamma))
+        if diff < diff_min:
+            diff_min = diff
+            result = gamma
+        gamma += 0.0001
+    print "(a) The gamma_0 is ", result
+    return result
+
+def rxtx_csi(x, gamma_0):
+    return log(x/gamma_0,2)*exp(-x/10)/10
+
+def question_b():
+    gamma_0 = question_a()
+    limit = 100
+    step = 0.0001
+    result = 0
+    delta = gamma_0
+    while delta < limit:
+        result += step*rxtx_csi(delta, gamma_0)
+        delta += step
+    print "(b) The integral result is ", result*10
+
+def rx_csi(x):
+    return log((x+1),2)*exp(-x/10)/10
+
+def question_d():
+    limit = 100
+    step = 0.0001
+    result = 0
+    delta = 0
+    while delta < limit:
+        result += step*rx_csi(delta)
+        delta += step
+    print "(d) The integral result is ", result*10
+
+
+if __name__ == "__main__":
+    question_b()
+    question_d()
