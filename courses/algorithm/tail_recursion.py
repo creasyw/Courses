9505c12d769a4f7c3e8e85c3eecf20d4be065813
creasyw <creasywuqiong@gmail.com>
2013-02-13 09:22:39 -0600

    Add anonymous function to deal with maximum recursion deepth

---
+++ courses/algorithm/tail_recursion.py
@@ -0,0 +1,13 @@
+def fac(n):
+    def f(n, acc):
+        return acc if n<2 else lambda: f(n-1, acc*n)
+    t = f(n, 1)
+    while callable(t):
+        t = t()
+    return t
+
+def test(n):
+    print fac(n)
+
+if __name__ == "__main__":
+    test(3000)
