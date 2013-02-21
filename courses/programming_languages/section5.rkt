a6e044c9c1ea3adfc6da3d25a592ea3a77e7ab39
creasyw <creasywuqiong@gmail.com>
2013-02-21 01:51:09 -0600

    Add lecture notes for section 5

--- courses/programming_languages/section5.rkt
+++ courses/programming_languages/section5.rkt
@@ -64,3 +64,54 @@
               (if (> tlans (car xs))
                   tlans
                   (car xs)))]))
+
+;; Delay and Force
+(define (my-delay th)
+  (mcons #f  th))
+(define (my-force p)
+  (if (mcar p)
+      (mcdr p)
+      (begin (set-mcar! p #t)
+             (set-mcdr! p ((mcdr p)))
+             (mcdr p))))
+
+;; Stream
+(define ones(lambda() (cons 1 ones)))
+(define (f x) (cons x (lambda () (f (+ x 1)))))
+(define powers-of-two
+  (letrec ([f  (lambda (x) (cons x (lambda () (f (* x 2)))))])
+    (lambda () (f 2))))
+
+; Another factorial with streaming
+(define (g acc x) (cons acc (lambda () (g (* x acc)(+ x 1)))))
+(define (fact2 stream n)
+  (letrec ([f (lambda (stream ans)
+             (let ([pr (stream)])
+               (if (>= ans n)
+                   (car pr)
+                   (f (cdr pr) (+ ans 1)))))])
+    (f stream 1)))
+; Test
+(fact2 (cdr (g 1 1)) 10)
+
+; Memozation
+(define fibonacci
+  (letrec ([memo null]
+           [f (lambda (x)
+                (let ([ans (assoc x memo)])
+                  (if ans
+                      (cdr ans)
+                      (let ([new-ans (if (or (= x 1) (= x 2))
+                                         1
+                                         (+ (f (- x 1))
+                                            (f (- x 2))))])
+                        (begin
+                          (set! memo (cons (cons x new-ans) memo))
+                          new-ans)))))])
+    f))
+
+; micro
+(define-syntax my-ifs
+  (syntax-rules (then else)
+    [(my-if e1 then e2 else e3)
+     (if e1 e2 e3)]))
