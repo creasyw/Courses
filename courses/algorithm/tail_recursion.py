296159c340918019932386c83064eace108f6ed9
creasyw <creasywuqiong@gmail.com>
2013-02-20 21:14:54 -0600

    Add generator for trying lazy evaluation

--- courses/algorithm/tail_recursion.py
+++ courses/algorithm/tail_recursion.py
@@ -7,10 +7,21 @@ def fac(n):
     return t
 
 def fac2(n):
-    return reduce(lambda x, y: x*y, xrange(1,num))
+    def f(n):
+        acc = 1
+        for i in xrange(1,n+1):
+            acc = acc *i
+            yield acc
+    t = f(n)
+    for i in xrange(n-1):
+        t.next()
+    return t.next()
+
+def fac3(n):
+    return reduce(lambda x, y: x*y, xrange(1,n+1))
 
 def test(n):
-    print fac(n)
+    print fac3(n)
 
 if __name__ == "__main__":
     test(3000)
