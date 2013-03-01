58d56393734b8fb823911c8441816aa4c108a939
creasyw <creasywuqiong@gmail.com>
2013-03-01 02:23:12 -0600

    Add randomness of select pivot in classic quicksort

--- courses/algorithm/quick_sort.py
+++ courses/algorithm/quick_sort.py
@@ -5,16 +5,22 @@ def quick_sort (arr):
         l[a] = l[b]
         l[b] = temp
         return l
+    def find_median(x):
+        candi = sorted([x[0],x[-1],x[int(ceil(len(x)/2.))-1]])[1]
+        return swap(x, 0, x.index(candi))
+    
     if len(arr)==0 or len(arr)==1:
         return arr
+    arr = find_median(arr)
     pivot = arr[0]
     i = 1
     for j in range(i,len(arr)):
         if arr[j]<pivot:
             arr = swap(arr, i, j)
             i += 1
-    arr = swap(arr, 0, i-1)
-    return quick_sort(arr[:i-1])+arr[i]+quick_sort(arr[i:])
+    i -= 1
+    arr = swap(arr, 0, i)
+    return quick_sort(arr[:i])+[arr[i]]+quick_sort(arr[i+1:])
         
 from math import ceil
 def quick_sort_count (arr, index, median=False):
@@ -47,29 +53,34 @@ def quick_sort_count (arr, index, median=False):
 def test (l):
     from random import shuffle
     x=range(l)
-    a = [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    c = [1, 11, 5, 15, 2, 12, 9, 99, 77, 0]
-    d = [999, 3, 2, 98, 765, 8, 14, 15, 16, 88, 145, 100]
-    e = [1, 11, 5, 15, 2, 999, 3, 2, 98, 765, 8, 14, 15, 16, 88, 145, 100, 12, 9, 99, 77, 0]
-    #shuffle(x)
-    #print "The original data set is ", x
-    arr, result = quick_sort_count(x, -1)
-    print "The overall comparison # is ", result
+    
+    # Test the quick_sort_count
+#    a = [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+#    b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#    c = [1, 11, 5, 15, 2, 12, 9, 99, 77, 0]
+#    d = [999, 3, 2, 98, 765, 8, 14, 15, 16, 88, 145, 100]
+#    e = [1, 11, 5, 15, 2, 999, 3, 2, 98, 765, 8, 14, 15, 16, 88, 145, 100, 12, 9, 99, 77, 0]
+#    arr, result = quick_sort_count(x, -1)
+#    print "The overall comparison # is ", result
+#
+#    arr, result = quick_sort_count(a, -1)
+#    print "The overall comparison # is ", result
+#    arr, result = quick_sort_count(b, -1)
+#    print "The overall comparison # is ", result
+#    arr, result = quick_sort_count(c, -1)
+#    print "The overall comparison # is ", result
+#    arr, result = quick_sort_count(d, -1)
+#    print "The overall comparison # is ", result
+#    arr, result = quick_sort_count(e, -1)
+#    print "The overall comparison # is ", result
 
-    arr, result = quick_sort_count(a, -1)
-    print "The overall comparison # is ", result
-    arr, result = quick_sort_count(b, -1)
-    print "The overall comparison # is ", result
-    arr, result = quick_sort_count(c, -1)
-    print "The overall comparison # is ", result
-    arr, result = quick_sort_count(d, -1)
-    print "The overall comparison # is ", result
-    arr, result = quick_sort_count(e, -1)
-    print "The overall comparison # is ", result
+    # Test the quick_sort
+    shuffle(x)
+    print "The original data set is ", x
+    print "The sorted array is ", quick_sort(x)
 
 if __name__ == "__main__":
-    test(100)
+    test(20)
 
 
 
