7bd5d5feed06385fa86cdf35616f1076cd3db434
creasyw <creasywuqiong@gmail.com>
2013-03-11 10:25:18 -0400

    Compelte the functionality of hw6

--- courses/programming_languages/hw6assignment.rb
+++ courses/programming_languages/hw6assignment.rb
@@ -16,21 +16,28 @@ class MyTetris < Tetris
   def key_bindings
     super
     @root.bind('u', proc{@board.rotate_halfcircle})
+    @root.bind('c', proc{@board.cheat})
   end
 
 end
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  Cheat_Piece = [[[0,0]]]	# for cheating
   Additional_Pieces = [rotations([[0,0],[1,0],[0,1]]), # tri
   		[[[0, 0], [-1, 0], [1, 0], [2, 0], [3,0]], #longest
   		[[0, 0], [0, -1], [0, 1], [0, 2], [0,3]]],
   		rotations([[0, 0], [1, 0], [0, 1], [1, 1],[2,0]]),
   		rotations([[0, 0], [1, 0], [0, 1], [1, 1],[2,1]])] 
   All_Pieces += Additional_Pieces
+
   # your enhancements here
   def self.next_piece (board)
-    MyPiece.new(All_Pieces.sample, board)
+    if !board.cheat_flag
+      MyPiece.new(All_Pieces.sample, board)
+    else
+      MyPiece.new(Cheat_Piece.sample, board)
+    end
   end
 
 end
@@ -40,17 +47,27 @@ class MyBoard < Board
   def initialize (game)
     super
     @current_block = MyPiece.next_piece(self)
+    @cheat_flag = false
   end
+  def cheat_flag
+    @cheat_flag
+  end
+
   def rotate_halfcircle
     if !game_over? and @game.is_running?
       @current_block.move(0, 0, 2)
     end
     draw
   end
+
   def next_piece
     @current_block = MyPiece.next_piece(self)
     @current_pos = nil
+    if @cheat_flag
+      @cheat_flag = false
+    end
   end
+  
   def store_current
     locations = @current_block.current_rotation
     displacement = @current_block.position
@@ -62,6 +79,14 @@ class MyBoard < Board
     }
     remove_filled
     @delay = [@delay - 2, 80].max
+  end
+  
+  def cheat
+    if !@cheat_flag and !game_over? and @game.is_running? and @score >=100
+      @cheat_flag = true
+      @score -= 100
+      @game.update_score
+    end
   end
 
 end
