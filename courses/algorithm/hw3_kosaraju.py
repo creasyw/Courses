1b5d127ac40274d5626598138399c7f2ad45356f
creasyw <creasywuqiong@gmail.com>
2013-03-18 22:38:38 -0500

    update hw3 solution

--- courses/algorithm/hw3_kosaraju.py
+++ courses/algorithm/hw3_kosaraju.py
@@ -4,6 +4,12 @@
 # where n and m  are the number of nodes and edges, respectively
 import sys, resource, time
 
+#Download the text file "kargerMinCut.txt".
+#The file contains the adjacency list representation of a simple undirected graph. There are 200 vertices labeled 1 to 200. The first column in the file represents the vertex label, and the particular row (other entries except the first column) tells all the vertices that the vertex is adjacent to. So for example, the 6th row looks like : "6  155 56  52  120 ......". This just means that the vertex with label 6 is adjacent to (i.e., shares an edge with) the vertices with labels 155,56,52,120,......,etc
+#
+#Your task is to code up and run the randomized contraction algorithm for the min cut problem and use it on the above graph to compute the min cut. (HINT: Note that you'll have to figure out an implementation of edge contractions. Initially, you might want to do this naively, creating a new graph from the old every time there's an edge contraction. But you should also think about more efficient implementations.) (WARNING: As per the video lectures, please make sure to run the algorithm many times with different random seeds, and remember the smallest cut that you ever find.) Write your numeric answer in the space provided. So e.g., if your answer is 5, just type 5 in the space provided.
+
+
 # Increase recursion limit and stack size
 sys.setrecursionlimit(2 ** 20)
 hardlimit = resource.getrlimit(resource.RLIMIT_STACK)[1]
@@ -11,7 +17,7 @@ resource.setrlimit(resource.RLIMIT_STACK,(hardlimit,hardlimit))
 
 # Establish source of directed graph, G
 # Source file has one directed edge per line, e.g. "1 5" is one line
-source = "SCC.txt" 
+source = "kargerMinCut.txt"
 # Number of nodes
 n = 875714 
 
