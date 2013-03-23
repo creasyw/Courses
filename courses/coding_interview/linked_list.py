c4c1fa47785bf2a7d57c19aeb93db2cc0a0b2fae
creasyw <creasywuqiong@gmail.com>
2013-03-23 17:09:48 -0500

    Add implementation of linked list in Python

---
+++ courses/coding_interview/linked_list.py
@@ -0,0 +1,35 @@
+import sys
+w = sys.stdout.write
+
+class Node:
+    def __init__(self, cargo=None, next=None):
+        self.car = cargo
+        self.cdr = next
+    def __str__(self):
+        return str(self.car)
+
+def printlst(lst):
+    if lst:
+        w("%s " %lst)
+        printlst(lst.cdr)
+    else:
+        w("nil\n")
+
+cons   = lambda el, lst: (el, lst)
+mklist = lambda *args: reduce(lambda lst, el: cons(el, lst), reversed(args), None)
+car = lambda lst: lst[0] if lst else lst
+cdr = lambda lst: lst[1] if lst else lst
+nth = lambda n, lst: nth(n-1, cdr(lst)) if n > 0 else car(lst)
+length  = lambda lst, count=0: length(cdr(lst), count+1) if lst else count
+begin   = lambda *args: args[-1]
+display = lambda lst: begin(w("%s " % car(lst)), display(cdr(lst))) if lst else w("nil\n")
+
+#Question:
+#Write a function that takes in an arbitrary length singly-linked list and
+#returns the pairwise reverse of that list. In other words, if I pass in the
+#list A->B->C->D->E->F the function should return the list B->A->D->C->F->E
+def pairwise_reverse(lst):
+    print "Hello world"
+
+
+
