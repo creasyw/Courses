05b3ea3b9fba9b187358c283b5efac79eb568bad
creasyw <creasywuqiong@gmail.com>
2013-03-24 10:47:31 -0500

    Add comments in racket linkedlist file

--- courses/coding_interview/linked_list.rkt
+++ courses/coding_interview/linked_list.rkt
@@ -12,6 +12,17 @@
 (define (delete x lst)
   (remove x lst))
 
+
+;; Questiuon:
+;; Write a function that takes in an arbitrary length singly-linked list and
+;; returns the pairwise reverse of that list. In other words, if I pass in the
+;; list A->B->C->D->E->F the function should return the list B->A->D->C->F->E
+;; 
+;; This is relatively easy. The most straightforward way is to use Lisp/Scheme/Racket,
+;; because the operations of "car" and "cdr" are conceptually alike the structure of 
+;; linkedlist. Besides, to make it more similiar to liinkedlist, I just need to
+;; implement several minor functions.
+
 (define (pair-reverse lst)
   (define (helper first rest)
     (cond [(null? rest) (cons first '())]
