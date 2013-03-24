2532499e744225e88185fa221441082efce3891e
creasyw <creasywuqiong@gmail.com>
2013-03-24 16:31:13 -0500

    the communication functioning well

--- courses/digital_communication/hw4.py
+++ courses/digital_communication/hw4.py
@@ -16,17 +16,56 @@ def signal_generation(Es):
     # Mapping, regardless of the grey coding
     signal[:,0] = map(lambda m: (Es)**0.5*cos(2*pi*m/8), rint)
     signal[:,1] = map(lambda m: (Es)**0.5*sin(2*pi*m/8), rint)
-    return signal
+    return rint, signal
 
 def transfer_in_channel (signal, Es, snr):
     """
-    The only input variable is the SNR value in LINEAR unit.
+    Simulate the effect that signal is transferring in the channel.
+    Note that the input variable SNR value is in LINEAR unit.
     """
     # Generate Gaussian noise with certain constraints
     awgn = np.random.normal(loc=0.0, scale=(Es/log(3,2)/snr/2)**0.5, size=len(signal))
     noise = (np.vstack((awgn, awgn))).T
     return signal+noise
 
+def detection (receive, Es):
+    """ Detect the received signal to symbol with MAP detector."""
+    cd = range(1,9)
+    # Mapping the codewords to modulation plane
+    mapping = (np.vstack((map(lambda m: (Es)**0.5*cos(2*pi*m/8), cd), \
+            map(lambda m: (Es)**0.5*sin(2*pi*m/8), cd)))).T
+    # calculate the norm distance between points
+    norm = lambda m: (reduce(lambda acc, itr: acc+itr**2, m, 0))**0.5
+    
+    decoded = []
+    for i in receive:
+        codeword = 0; mindis = float('+inf')
+        # Performing nearest neighbor detection
+        for j in range(len(mapping)):
+            distance = norm(i-mapping[j])
+            if distance < mindis:
+                mindis = distance
+                # j is the index of mapping, the real codeword should be j+1
+                codeword = j+1
+        assert codeword!=0, "There is something wrong in the decoding process!"
+        decoded.append(codeword)
+    return decoded
 
+def performance(tx, rx):
+    """ Calculate the symbol error rate. """
+    count = sum(1 for i in range(len(tx)) if tx[i]==rx[i])
+    return float(count)/len(tx)
 
+def main():
+    Es = 1
+    snr = range(0,19)
+    tx, signal = signal_generation(Es)
+    for i in snr:
+        rx = transfer_in_channel(signal, Es, 10**(i/10.))
+        decoded = detection(rx, Es)
+        ser = performance(tx, decoded)
+        print "SNR=%s -- SER=%s."%(i, ser)
+
+if __name__=="__main__":
+    main()
 
