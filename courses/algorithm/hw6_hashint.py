f5f7e959cfd99fa1fbcdc2f85e2c54f5e60406e4
creasyw <creasywuqiong@gmail.com>
2013-03-27 00:58:00 -0500

    Add discription of the hws

--- courses/algorithm/hw6_hashint.py
+++ courses/algorithm/hw6_hashint.py
@@ -1,3 +1,18 @@
+#The goal of this problem is to implement a variant of the 2-SUM algorithm (covered in the Week 6 lecture on hash table applications).
+#The file contains 500,000 positive integers (there might be some repetitions!).This is your array of integers, with the ith row of the file specifying the ith entry of the array.
+#
+#Your task is to compute the number of target values t in the interval [2500,4000] (inclusive) such that there are distinct numbers x,y in the input file that satisfy x+y=t. (NOTE: ensuring distinctness requires a one-line addition to the algorithm from lecture.)
+#
+#Write your numeric answer (an integer between 0 and 1501) in the space provided.
+#
+#
+#OPTIONAL COMMENT: You might notice that the chosen targets are relatively small numbers. (There is a good reason for this. Can you guess what would be the problem with a similar interval of larger targets?) You are welcome to add extra optimizations that take advantage of this fact, though it is not required for the assignment.
+#
+#OPTIONAL CHALLENGE: If this problem is too easy for you, try implementing your own hash table for it. For example, you could compare performance under the chaining and open addressing approaches to resolving collisions.
+#
+#REQUEST FOR COMMENTS: Do you have a favorite hash table application that could serve as a programming assignment for this course? Post your idea to the discussion form, and maybe it will be used in the next iteration of this course!
+
+
 # use hashing, use chains to deal with conflicts
 def loadData():
     MAX = 0
