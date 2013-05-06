771f6280ec3b949891bedec8405c73a2e4ac12d6
creasyw <creasywuqiong@gmail.com>
2013-05-06 12:17:00 -0500

    Add zero forcing equalizer

--- courses/digital_communication/equalizer.py
+++ courses/digital_communication/equalizer.py
@@ -4,7 +4,7 @@ from math import sqrt
 tap1 = np.array([0.227, 0.45, 0.7, 0.45, 0.227])
 tap2 = np.array([0.41, 0.815, 0.41])
 
-def awgn(nsample):
+def awgn(nsample, nvar):
     return np.random.normal(loc=0, scale=sqrt(nvar), size=nsample)
 
 def binary_pam(nsample):
@@ -19,5 +19,23 @@ def channel(signal, tap):
         for k in range(len(tap)):
             if n-k >= 0: output[n] += tap[k]*signal[n-k]
     return output
+
+def zero_forcing(nchannel, nsample, nzf, snr):
+    if nchannel == 1:
+        tap = tap1
+    elif nchannel == 2:
+        tap = tap2
+    else:
+        raise ValueError("The channel shoudl be either 1 or 2!")
+    
+    index = np.zeros((nzf, nzf), dtype=float)
+    q = np.zeros(nzf, dtype=float)
+    q[nzf/2+1] = 1
+
+    for n in range(len(index)):
+        for j in range(n, n-3, -1):
+            if j<0 or j>=nzf: continue
+            index[n, j] = tap[n-j]
+    print np.linalg.solve(index, q)
 
 
