8c32c75e73c5893f2c2b7997e56acd861124404c
creasyw <creasywuqiong@gmail.com>
2013-05-14 20:23:26 -0500

    Change plot settings

--- courses/machine_learning/ex1/plotData.m
+++ courses/machine_learning/ex1/plotData.m
@@ -16,10 +16,13 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
+data = load('ex1data1.txt');       % read comma separated data
+x = data(:, 1); y = data(:, 2);
+m = length(y);                     % number of training examples
 
-
-
-
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('Profit in $10,000s'); % Set the y−axis label
+xlabel('Population of City in 10,000s'); % Set the x−axis label
 
 % ============================================================
 
