90818c03046308b73e6414b6906072e469172595
creasyw <creasywuqiong@gmail.com>
2013-09-21 18:00:05 -0500

    sol to q01

---
+++ courses/algo_analysis_II/hw3/knapsack.py
@@ -0,0 +1,31 @@
+import os
+import re
+import numpy as np
+
+finder = re.compile("-?\d+")
+
+def knapsack(arr, weight, size):
+    result = np.zeros((size, weight+1))
+    for k in range(arr[0,1],weight+1):
+        result[0,k] = arr[0,0]
+    for i in range(1, size):
+        for j in range(weight+1):
+            if j-arr[i,1] >=0:
+                result[i,j] = max( result[i-1, j], result[i-1, j-arr[i,1]]+arr[i,0] )
+            else:
+                result[i,j] = result[i-1, j]
+    return result[-1,-1]
+
+def main():
+    temp = []
+    with open(os.path.join(os.path.dirname(__file__), "q01_toy3.txt")) as datafile:
+            weight, size = [int(k) for k in finder.findall(datafile.readline())]
+            for row in datafile:
+                temp.append([int(k) for k in finder.findall(row)])
+    temp = np.array(temp)
+    print knapsack(temp, weight, size)
+
+if __name__ == "__main__":
+    main()
+
+
