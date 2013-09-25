85835fd23be97083da2bbb7a8d5cb3ebb2f5644b
creasyw <creasywuqiong@gmail.com>
2013-09-25 00:33:23 -0500

    optimize calculation data switching

--- courses/algo_analysis_II/hw3/knapsack_big.py
+++ courses/algo_analysis_II/hw3/knapsack_big.py
@@ -5,20 +5,19 @@ import numpy as np
 finder = re.compile("-?\d+")
 
 def knapsack(arr, weight, size):
-    print weight
-    print size
-    previous = np.zeros(weight+1)
+    data = np.zeros((2, weight+1))
+    count = 1
     for k in range(arr[0,1],weight+1):
-        previous[k] = arr[0,0]
+        data[0,k] = arr[0,0]
     for i in range(1, size):
-        current = np.zeros(weight+1)
         for j in range(weight+1):
+            previous = count ^ 1
             if j-arr[i,1] >=0:
-                current[j] = max(previous[j], previous[j-arr[i,1]]+arr[i,0] )
+                data[count, j] = max(data[previous, j], data[previous, j-arr[i,1]]+arr[i,0] )
             else:
-                current[j] = previous[j]
-        previous = current
-    return current[-1]
+                data[count, j] = data[previous, j]
+        count = previous
+    return int(data[count ^ 1, -1])
 
 def main():
     temp = []
