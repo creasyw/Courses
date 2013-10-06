7bd6769eab4dc0591725c20aa20d4a9845eef0b5
creasyw <creasywuqiong@gmail.com>
2013-10-06 16:56:24 -0500

    read file

---
+++ courses/algo_analysis_II/hw5/tsp.py
@@ -0,0 +1,22 @@
+import os, re
+import numpy as np
+
+finder = re.compile("-?\d+.?\d*")
+
+
+def main():
+    import sys
+    assert len(sys.argv)==2, "The proper input format is: ~$ python SCRIPT.py data_file start_node"
+    filename = sys.argv[1]
+    data = []
+    with open(os.path.join(os.path.dirname(__file__), filename)) as datafile:
+            num = int(datafile.readline())
+            for row in datafile:
+                data.append([float(k) for k in finder.findall(row)])
+    
+    print np.array(data)
+
+if __name__ == "__main__":
+    main()
+
+
