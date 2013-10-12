1c57d0e27d8fa0fed12193c757963fa82b2d42e2
creasyw <creasywuqiong@gmail.com>
2013-10-12 14:59:46 -0500

    add week4 func-object example

---
+++ courses/fp_scala/notes/List.scala
@@ -0,0 +1,22 @@
+package week4
+
+trait List[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+class Nil[T] extends List[T] {
+  def isEmpty: Boolean = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object List {
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+}
