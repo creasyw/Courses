4ac70d1d8a6500b123e70628d882c63772233cfe
creasyw <creasywuqiong@gmail.com>
2013-10-15 21:43:05 -0500

    add name of hw

--- courses/algo_analysis_II/hw6_2sat/sat.py
+++ courses/algo_analysis_II/hw6_2sat/sat.py
@@ -17,6 +17,7 @@ def preprocess(clause, num):
             l2 = np.where(benchmark==abs(item[1]))[0][0]
             c1 = list(new_clause[l1])
             c2 = list(new_clause[l2])
+            #print count, l1, l2
             #print c1, c2
             # the same variables have been in the same clause
             if l1 == l2:
@@ -73,8 +74,22 @@ def preprocess(clause, num):
         count += 1
 
     new_clause = np.array(filter(lambda x: x[0]!=0, list(new_clause)))
-    new_num = len(set(k for k in new_clause.flat if k!=0))
-    return np.array(new_clause), new_num
+    new_var = set(k for k in new_clause.flat if k!=0)
+    print len(new_clause), len(new_var)
+    #print new_clause
+    #print new_var
+    if len(new_var) == 2*len(new_clause):
+        return np.array([[1,-1]]), 2
+    else:
+        freq = np.bincount(np.abs(new_clause).flat)
+        benchmark = abs(new_clause)
+        for i in range(len(freq)):
+            if freq[i] == 1:
+                l = np.where(benchmark==i)[0][0]
+                new_clause[l] = [0,0]
+        new_clause = np.array(filter(lambda x: x[0]!=0, list(new_clause)))
+        new_var = set(k for k in new_clause.flat if k!=0)
+        return new_clause, len(new_var)
 
 
 
@@ -119,7 +134,7 @@ def main():
                  count += 1
 
     clause, new_num =  preprocess(clause, num)
-    print len(clause), new_num
+    print clause
     print sat(clause, num, new_num)
 
 if __name__ == "__main__":
