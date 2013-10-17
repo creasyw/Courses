904624f7d655ba2035dfaa2560584a1ddd48d967
creasyw <creasywuqiong@gmail.com>
2013-10-17 16:42:19 -0500

    add recursive and pattern matching

--- misc/test.hs
+++ misc/test.hs
@@ -5,3 +5,17 @@ square x = x * x
 
 cubic :: Floating a => a -> a
 cubic a = a * a* a
+
+factorial :: (Integral a) => a -> a
+factorial x
+  | x <= 1 = 1
+  | otherwise = x * factorial(x - 1)             
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
