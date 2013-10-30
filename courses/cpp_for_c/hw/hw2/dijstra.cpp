9500733a172e12e9107523d2a75929b7b30d762f
creasyw <creasywuqiong@gmail.com>
2013-10-30 00:10:41 -0400

    without rookie bugs...

--- courses/cpp_for_c/hw/hw2/dijstra.cpp
+++ courses/cpp_for_c/hw/hw2/dijstra.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
+using namespace std;
+
 
 class graph{
   public:
-    graph(int n) num = n;
+    graph(int n) {
+        num = n;
+    }
 
-    void display_matrix(const float& m, int nr, int nc){
-      for (int i=0; i <= nr; ++i){
-	  for (int j=0; j <= nc; ++j)
-	    cout<< m[i][j] << " ";
-	  cout << endl;
-      }
+    void display_matrix(int nc, int nr, float (&array)[nc][nr]){
+        for (int i=0; i <= nc; ++i){
+	    for (int j=0; j <= nr; ++j)
+		cout<< array[i][j] << " ";
+	    cout << endl;
+	}
     }
 
   private:
     int num;
     
+};
+
+int main()
+{
+    cout << "fuck c++" << endl;
 }
-
-
 
