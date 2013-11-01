279906d9f5ac1581167295b1ac7e10b062df83a6
creasyw <creasywuqiong@gmail.com>
2013-11-01 08:42:12 -0500

    update heapitem and add special swap

--- courses/cpp_for_c/hw/hw2/minheap.cpp
+++ courses/cpp_for_c/hw/hw2/minheap.cpp
@@ -6,39 +6,53 @@ using namespace std;
 class heapitem {
     public:
         // initial func
-        void heapitem(float k, int x, int y) {
+        void heapitem(float k, int x) {
             key = k;
-            node1 = x;
-            node2 = y;
+            node = x;
         }
 
-        float getter() {
+        float get_key() {
             return key;
         }
-        void setter(float k) {
+        void set_key(float k) {
             key = k;
+        }
+        float get_node() {
+            return node;
+        }
+        void set_node(int x) {
+            node = x;
         }
     
     private:
         float key; // key is the up-to-date distance
-        int node1;
-        int node2
+        int node;
 };
 
 // class for the operations of entire heap
 class minheap {
     public:
         void heap(int size) {
-            elem = new float[size];
+            elem = new heapitem[size];
             length = size;
         }
 
         // test if the heap contains certain element
-        bool contains(int k) {
+        bool contains(float k) {
             for(int i=0; i<length; i++) {
                 if (elem[i]==key) return true;
             }
             return false;
+        }
+
+        void heap_swap(const heapitem &h1, const heapitem &h2) {
+            heapitem temp(0,0);
+            temp.set_key(h1.get_key());
+            temp.set_node(h1.get_node());
+            h1.set_key(h2.get_key());
+            h1.set_node(h2.get_node());
+            h2.set_key(temp.get_key());
+            h2.set_node(temp.set_node());
         }
 
         // main func: pop the minimum element out of the heap
@@ -51,12 +65,12 @@ class minheap {
                 cerr << "Heap Empty" << e.what() << endl;
             }
 
-            float result = elem[0];
+            heapitem result = elem[0];
             int current = 0;
             int child = 2;
             while (child<length) {
-                if (elem[child-1] < elem[child]) {
-                    swap(elem[current], elem[child-1]);
+                if (elem[child-1].get_key() < elem[child].get_key()) {
+                    heap_swap(elem[current], elem[child-1]);
                     current = child-1;
                     child = (child-1)*2;
                 } else {
@@ -69,6 +83,10 @@ class minheap {
                 swap(elem[current], elem[length-1]);
             length -= 1;
         }
+
+        // update the key value of the element in the heap
+
+        // push new element into the heap
 
                 
     private:
