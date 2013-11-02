63225b3a2e9c15cbbfc288f3ae88eb694fb1f60c
creasyw <creasywuqiong@gmail.com>
2013-11-02 00:30:11 -0500

    add header for minheap

---
+++ courses/cpp_for_c/hw/hw2/minheap.h
@@ -0,0 +1,39 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include "minheap.h"
+
+// class for a node in heap
+class heapitem {
+    public:
+        heapitem();
+        heapitem(float k, int x);
+        float get_key();
+        void set_key(float k);
+        int get_node();
+        void set_node(int x);
+    private:
+        float key;
+        int node;
+};
+
+// class for the operations of entire heap
+class minheap {
+    public:
+        minheap(int n);
+        void display();
+        bool contains(int n);
+        int size();
+        heapitem pop();
+        void update(float k, int n);
+        void push(float k, int n);
+
+    private:
+        heapitem *elem;
+        int num;            // the num of element
+        int length;         // size of the array
+        void heap_swap(heapitem &h1, heapitem &h2);
+        int find(int n);
+};
+
+#endif
