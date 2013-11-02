7548f06d31dcc8a733413796cc966fe0c97489b2
creasyw <creasywuqiong@gmail.com>
2013-11-02 16:24:18 -0500

    separate declearation

--- courses/cpp_for_c/hw/hw2/dijstra.cpp
+++ courses/cpp_for_c/hw/hw2/dijstra.cpp
@@ -1,139 +1,130 @@
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+#include "dijstra.h"
 
 using namespace std;
 
+graph::graph(int n) {
+    num = n;
+    arr = new int*[num];
+    for (int i=0; i<num; ++i)
+        arr[i] = new int[num];
+}
 
-class graph{
-    public:
-        // The graph is stored in a matrix
-        graph(int n) {
-            num = n;
-            arr = new int*[num];
-            for (int i=0; i<num; ++i)
-                arr[i] = new int[num];
-        }
+// return number of vertices
+int graph::num_of_vertices() {
+    return num;
+}
 
-        // return number of vertices
-        int num_of_vertices() {
-            return num;
-        }
+// This is specific for the undirected graph
+int graph::num_of_edges() {
+    int count = 0;
+    for (int i=0; i < num-1; ++i)
+        for (int j = i+1; j < num; ++j)
+            if (arr[i][j] > 0) count++;
+    return count;
+}
 
-        // This is specific for the undirected graph
-        int num_of_edges() {
-            int count = 0;
-            for (int i=0; i < num-1; ++i)
-                for (int j = i+1; j < num; ++j)
-                    if (arr[i][j] > 0) count++;
-            return count;
-        }
+// tests whether there is an edge from node x to node y.
+bool graph::adjancent(int x, int y) {
+    check(x, -1, num-1, "index of vertex is out of range");
+    check(y, -1, num-1, "index of vertex is out of range");
+    return arr[x][y]>0;
+}
 
-        // tests whether there is an edge from node x to node y.
-        bool adjancent(int x, int y) {
-            check(x, -1, num-1, "index of vertex is out of range");
-            check(y, -1, num-1, "index of vertex is out of range");
-            return arr[x][y]>0;
-        }
-        
-        // lists all nodes y such that there is an edge from x to y
-        vector<int> neighbors(int x) {
-            check(x, -1, num-1, "index of vertex is out of range");
+// lists all nodes y such that there is an edge from x to y
+vector<int> graph::neighbors(int x) {
+    check(x, -1, num-1, "index of vertex is out of range");
 
-            vector<int> result;
-            for (int i=0; i<num; ++i) {
-                if (arr[x][i] > 0) result.push_back(i);
-            }
-            return result;
-        }
+    vector<int> result;
+    for (int i=0; i<num; ++i) {
+        if (arr[x][i] > 0) result.push_back(i);
+    }
+    return result;
+}
 
-        // adds to G the edge from x to y with value val, if it does not exist
-        void add (int x, int y, int val) {
-            check(x, -1, num-1, "index of vertex is out of range");
-            check(y, -1, num-1, "index of vertex is out of range");
+// adds to G the edge from x to y with value val, if it does not exist
+void graph::add (int x, int y, int val) {
+    check(x, -1, num-1, "index of vertex is out of range");
+    check(y, -1, num-1, "index of vertex is out of range");
 
-            if (arr[x][y] > 0 || x == y) return;
-            arr[x][y] = arr[y][x] = val;
-        }
+    if (arr[x][y] > 0 || x == y) return;
+    arr[x][y] = arr[y][x] = val;
+}
 
-        // removes the edge from x to y, if it is there
-        void remove (int x, int y) {
-            arr[x][y]=0;
-        }
+// removes the edge from x to y, if it is there
+void graph::remove (int x, int y) {
+    arr[x][y]=0;
+}
 
-        // display matrix in a row/col format
-        void display_matrix(){
-            for (int i=0; i < num; ++i) {
-                for (int j=0; j < num; ++j)
-                cout<< arr[i][j] << " ";
-                cout << endl;
-            }
-        }
+// display matrix in a row/col format
+void graph::display_matrix(){
+    for (int i=0; i < num; ++i) {
+        for (int j=0; j < num; ++j)
+        cout<< arr[i][j] << " ";
+        cout << endl;
+    }
+}
 
-        void directed_matrix(float density, int lrange, int urange) {
-            check(density, 0, 1, "Density should be in (0,1]");
-            check(lrange, 0, urange, "The ranges is 0 < lrange< urange");
+void graph::directed_matrix(float density, int lrange, int urange) {
+    check(density, 0, 1, "Density should be in (0,1]");
+    check(lrange, 0, urange, "The ranges is 0 < lrange< urange");
 
-            int nedge = static_cast<int>(num*(num-1)*density);
-            int count = 0;
-            cout << nedge << endl;
-            while (count < nedge) {
-                srand(clock());
-                int col = rand() % num;
-                int row = rand() % num;
-                // if the route has been set, it doesn't need to set again.
-                // Or, there is no sense to set diagonal elements
-                if (arr[row][col] > 0 || row == col)
-                    continue;
-                int val = rand() % (urange - lrange + 1) + lrange;
-                arr[row][col] = val;
-                count++;
-            }
-        }
-        
-        // The undirected matrix has the same value for both [i,j] and [j,i]
-        void undirected_matrix(float density, int lrange, int urange) {
-            check(density, 0, 1, "Density should be in (0,1]");
-            check(lrange, 0, urange, "The ranges is 0 < lrange< urange");
+    int nedge = static_cast<int>(num*(num-1)*density);
+    int count = 0;
+    cout << nedge << endl;
+    while (count < nedge) {
+        srand(clock());
+        int col = rand() % num;
+        int row = rand() % num;
+        // if the route has been set, it doesn't need to set again.
+        // Or, there is no sense to set diagonal elements
+        if (arr[row][col] > 0 || row == col)
+            continue;
+        int val = rand() % (urange - lrange + 1) + lrange;
+        arr[row][col] = val;
+        count++;
+    }
+}
 
-            int nedge = static_cast<int>(num*(num-1)/2*density);
-            cout << num*(num-1)/2 << endl;
-            cout << nedge << endl;
-            int count = 0;
-            while (count < nedge) {
-                srand(clock());
-                int row = rand() % num;
-                int col = rand() % (num-row);
-                // if the route has been set, it doesn't need to set again.
-                // Or, there is no sense to set diagonal elements
-                if (arr[row][col] > 0 || row == col)
-                    continue;
-                int val = rand() % (urange - lrange + 1) + lrange;
-                arr[row][col] = arr[col][row] = val;
-                count++;
-            }
-        }
+// The undirected matrix has the same value for both [i,j] and [j,i]
+void graph::undirected_matrix(float density, int lrange, int urange) {
+    check(density, 0, 1, "Density should be in (0,1]");
+    check(lrange, 0, urange, "The ranges is 0 < lrange< urange");
 
-    private:
-        int num;
-        int**  arr;
+    int nedge = static_cast<int>(num*(num-1)/2*density);
+    cout << num*(num-1)/2 << endl;
+    cout << nedge << endl;
+    int count = 0;
+    while (count < nedge) {
+        srand(clock());
+        int row = rand() % num;
+        int col = rand() % (num-row);
+        // if the route has been set, it doesn't need to set again.
+        // Or, there is no sense to set diagonal elements
+        if (arr[row][col] > 0 || row == col)
+            continue;
+        int val = rand() % (urange - lrange + 1) + lrange;
+        arr[row][col] = arr[col][row] = val;
+        count++;
+    }
+}
 
-        // sanity check for arguments
-        //  The checking range is lower < target <= upper
-        void check(float target, int lower, int upper, string e) {
-            try {
-                if (target <= lower || target > upper)
-                    throw invalid_argument(e);
-            }
-            catch (const invalid_argument& ia) {
-                cerr << "Invalid argument: " << ia.what() << endl;
-                exit (EXIT_FAILURE);
-            }
-        }
-            
-            
-    
-};
+// sanity check for arguments
+//  The checking range is lower < target <= upper
+void graph::check(float target, int lower, int upper, string e) {
+    try {
+        if (target <= lower || target > upper)
+            throw invalid_argument(e);
+    }
+    catch (const invalid_argument& ia) {
+        cerr << "Invalid argument: " << ia.what() << endl;
+        exit (EXIT_FAILURE);
+    }
+}
+
+
 
 int main()
 {
