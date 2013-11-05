a80bfb9dbdd812d11c9ba094e3e635108df99d00
creasyw <creasywuqiong@gmail.com>
2013-11-04 19:58:24 -0600

    complete object

--- courses/fp_scala/notes/Pouring.scala
+++ courses/fp_scala/notes/Pouring.scala
@@ -31,12 +31,32 @@ class Pouring(capacity: Vector[Int]) {
       (for (from <- glasses; to <- glasses if from != to) yield Pour(from, to))
 
   // Paths
-  class Path(history:List[Move]) {
-    def endState: State = trackState(history)
-    private def trackState(xs:List[Move]): State = xs match {
-      case Nil => initialState
-      case move :: xs1 => move change trackState(xs1)
-    }
+  class Path(history: List[Move]) {
+    def endState: State = (history foldRight initialState)(_ change _)
+    def extend(move: Move) = new Path(move :: history)
+    override def toString = (history.reverse mkString " ") + "--> " + endState
   }
+
+  val initialPath = new Path(Nil)
+
+  def from(paths: Set[Path], explored: Set[State]): Stream[Set[Path]] =
+    if (paths.isEmpty) Stream.empty
+    else {
+      val more = for {
+        path <- paths
+        next <- moves map path.extend
+        if !(explored contains next.endState)
+      } yield next
+      paths #:: from(more, explored ++ (more map (_.endState)))
+    }
+
+  val pathSets = from(Set(initialPath), Set(initialState))
+
+  def solution(target: Int): Stream[Path] =
+    for {
+      pathSet <- pathSets
+      path <- pathSet
+      if path.endState contains target
+    } yield path
 
 }
