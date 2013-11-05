1dc36d5a1dd4eaa65d2d2455742632512f54752f
creasyw <creasywuqiong@gmail.com>
2013-11-05 17:05:42 -0600

    implement terrian functions

--- courses/fp_scala/hw/hw6/main/scala/streams/StringParserTerrain.scala
+++ courses/fp_scala/hw/hw6/main/scala/streams/StringParserTerrain.scala
@@ -5,24 +5,24 @@ import common._
 /**
  * This component implements a parser to define terrains from a
  * graphical ASCII representation.
- * 
+ *
  * When mixing in that component, a level can be defined by
  * defining the field `level` in the following form:
- * 
+ *
  *   val level =
  *     """------
  *       |--ST--
  *       |--oo--
  *       |--oo--
  *       |------""".stripMargin
- * 
+ *
  * - The `-` character denotes parts which are outside the terrain
  * - `o` denotes fields which are part of the terrain
  * - `S` denotes the start position of the block (which is also considered
-     inside the terrain)
+ * inside the terrain)
  * - `T` denotes the final position of the block (which is also considered
-     inside the terrain)
- * 
+ * inside the terrain)
+ *
  * In this example, the first and last lines could be omitted, and
  * also the columns that consist of `-` characters only.
  */
@@ -38,21 +38,24 @@ trait StringParserTerrain extends GameDef {
    * This method returns terrain function that represents the terrain
    * in `levelVector`. The vector contains parsed version of the `level`
    * string. For example, the following level
-   * 
+   *
    *   val level =
    *     """ST
    *       |oo
    *       |oo""".stripMargin
-   * 
+   *
    * is represented as
-   * 
+   *
    *   Vector(Vector('S', 'T'), Vector('o', 'o'), Vector('o', 'o'))
    *
    * The resulting function should return `true` if the position `pos` is
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean =
+    p => if (p.x < 0 || p.y < 0) false
+    else if (p.x >= levelVector.length || p.y >= levelVector(p.x).length) false
+    else levelVector(p.x)(p.y) != '-'
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +65,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector indexWhere (_ contains c)
+    val y = levelVector(x) indexWhere (_ == c)
+    Pos(x, y)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
