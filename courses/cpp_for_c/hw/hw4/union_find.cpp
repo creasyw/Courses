5b244d30f1544c3542e70e256687af948ebe8470
creasyw <creasywuqiong@gmail.com>
2013-11-15 10:51:17 -0600

    change union_find for hex-board data

--- courses/cpp_for_c/hw/hw4/union_find.cpp
+++ courses/cpp_for_c/hw/hw4/union_find.cpp
@@ -12,32 +12,48 @@ int union_find::find(int i) {
         return -1;
     return it->second;
 }
+
+// merge nodes with leader of l2 to l1
 void union_find::unions(int l1, int l2) {
-    vector<int> temp = leader[l2];
-    for(vector<int>::iterator it=temp.begin(); it!=temp.end(); ++it)
-        nodes[*it] = l1;
+    // to simplify the procedure of insert
+    if (l1==l2) return;
+    // merge two set together
+    vector<int>temp = leader[l2];
+    for(auto it : temp)
+        nodes[it] = l1;
     leader[l1].insert(leader[l1].begin(),leader[l2].begin(),leader[l2].end());
     leader.erase(l2);
 }
-// the input two nodes i and j are presumed having different leaders
-void union_find::insert(int i, int j) {
-    int l1 = find(i);
-    int l2 = find(j);
-    if (l1!=-1 && l2!=-1)
-        unions(min(l1, l2) ,max(l1, l2));
-    else if(l1!=-1) {
-        nodes[j] = l1;
-        leader[l1].push_back(j);
-    } else if(l2!=-1) {
-        nodes[i] = l2;
-        leader[l2].push_back(i);
+
+// assuming the player0 plays vertically and player1 plays horizontally
+// hence, player0's leader indicator is the # of row, and player1 is the # of column
+void union_find::insert(vector<int> n, vector<int> nbs, int player) {
+    int index;
+    // the "universal" rul mapping coordinates to node indices
+    int n_val = n[0]*100+n[1];
+    if (player==0)
+        index = n[0];
+    else
+        index = n[1];
+    vector<int> leaders;
+    for (auto it: nbs) {
+        int l = find(it);
+        if (l!=-1)  leaders.push_back(l);
+    }
+    if (leaders.size()==0) {
+        nodes[n_val] = index;
+        leader[index] = vector<int>();
+        leader[index].push_back(n_val);
     } else {
-        int l = min(i,j);
-        nodes[i] = l;
-        nodes[j] = l;
-        leader[l] = vector<int>();
-        leader[l].push_back(i);
-        leader[l].push_back(j);
+        int new_leader = *min_element(leaders.begin(), leaders.end());
+        if (new_leader > index) {
+            leader[index] = vector<int>();
+            new_leader = index;
+        }
+        for (auto it: leaders)
+            unions(new_leader, it);
+        nodes[n_val] = new_leader;
+        leader[new_leader].push_back(n_val);
     }
 }
 
--- courses/cpp_for_c/hw/hw4/union_find.h
+++ courses/cpp_for_c/hw/hw4/union_find.h
@@ -1,8 +1,8 @@
 #ifndef UNIONFIND_H
 #define UNIONFIND_H
 
-#include<unordered_map>
-#include<vector>
+#include <unordered_map>
+#include <vector>
 #include "union_find.h"
 
 using namespace std;
@@ -10,13 +10,15 @@ using namespace std;
 // data structure used in Kruskal's algorithm
 class union_find {
     public:
+        inline union_find(int n): last(n) {}
         inline int num_of_unions() {return leader.size();}
         int find(int i);
         void unions(int l1, int l2);
-        void insert(int i, int j);
+        void insert(vector<int> n, vector<int> ns, int p);
     private:
+        int last;
         // the 1st element is the node, and the 2nd is the leader of cluster
-        unordered_map<int, int> nodes;
+        unordered_map<int, int > nodes;
         // the 1st element is the leader, and the 2nd are the nodes
         unordered_map<int, vector<int> > leader;
 };
