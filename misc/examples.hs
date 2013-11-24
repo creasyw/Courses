440380e8aa52146a2d91e1156bddef352aeffb33
creasyw <creasywuqiong@gmail.com>
2013-11-24 15:33:10 -0500

    add custumized data structure

--- misc/examples.hs
+++ misc/examples.hs
@@ -51,3 +51,11 @@ longerThan n upperbound
 
 -- use "scanl" to monitor the progress of accumulating
 sqrtSum1 = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) +1
+
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi* r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
