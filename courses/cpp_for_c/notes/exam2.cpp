3897787fbd2adfdaf5f6d0283b11fae7a1deaa6f
creasyw <creasywuqiong@gmail.com>
2013-12-07 21:13:58 -0600

    mocking final2

---
+++ courses/cpp_for_c/notes/exam2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+ 
+int main()
+{
+       int a = 3, b = -2, c = 2, d = 4;
+       cout<< sqrt(d * 1.0)  << endl;
+       cout<< a - 5 / 2  << endl;
+       cout<< c++ * -a << endl;
+       cout<< d * ++b << endl;
+       cout<<  rand() % 5 / 10  << endl;
+}
