509985f534bec89beccd30b8138bd73f87046ce1
creasyw <creasywuqiong@gmail.com>
2013-12-19 00:22:40 -0600

    del testing file

--- courses/learn_haskell/readfile.hs
+++ courses/learn_haskell/readfile.hs
@@ -2,4 +2,11 @@ import Data.Char
 
 main = do
   contents <-getContents
-  putStr $ map toUpper contents
+  putStr $ shortLinesOnly contents
+
+shortLinesOnly input =
+  let allLines = lines input
+      shortLines = filter (\line -> length line < 10) allLines
+      result = unlines shortLines
+  in  result
+      
