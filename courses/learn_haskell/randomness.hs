ed11c58c1f8e70c6ae04c8ec7a6e37ea1017a29e
creasyw <creasywuqiong@gmail.com>
2014-01-05 00:58:50 -0600

    works fine by correcting func declaration

--- courses/learn_haskell/randomness.hs
+++ courses/learn_haskell/randomness.hs
@@ -7,8 +7,9 @@ threeCoins gen =
       (third, new'') = random new'
   in (first, second, third)
 
-finiteRandoms :: (Num n, Eq n, RandomGen g, Random a) => n -> g -> ([a], g)
-finiteRandoms 0 gen = ([], gen)
+
+finiteRandoms :: Int -> StdGen -> ([Int], StdGen)
+finiteRandoms 0 g = ([], g)
 finiteRandoms n gen =
   let (value, newGen) = random gen
       (restOfList, finalGen) = finiteRandoms (n-1) newGen
