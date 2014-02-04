41b6eeabf75217606ac6ef8fecc014d7a9f31285
creasyw <creasywuqiong@gmail.com>
2014-02-04 13:36:26 -0600

    add func to test if nash equilibrium exists

---
+++ courses/game_theory/maxmin.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def nash_equilibrium(matrix):
+    """ 
+    input - matrix: np.ndarray
+    output- has_ne: bool. whether the N.E. exists
+    """
+    # minimax of col == maximin of row
+    return matrix.max(0).min()==matrix.min(1).max
