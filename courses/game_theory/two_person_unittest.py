448d31d394944ca050c50b5fe17f5f854982a8f0
creasyw <creasywuqiong@gmail.com>
2014-02-10 13:08:16 -0600

    correct unittest...

--- courses/game_theory/two_person_unittest.py
+++ courses/game_theory/two_person_unittest.py
@@ -3,12 +3,17 @@ import unittest
 import numpy as np
 
 class BasicMatrixGame(unittest.TestCase):
-    def test_cases(self):
+    def setUp(self):
         self.m1 = np.ones((5,3))
         self.m1[2][1] = 5
+        # m2 does not have N.E. point
+        self.m2 = np.array([[3,-1],[-1,9]])
 
-    def nash_test(self):
-        self.assertTrue(nash_equilibrium(self.m1)==True)
+    def test_nash_true(self):
+        self.assertTrue(mm.nash_equilibrium(self.m1)==True)
+
+    def test_nash_fail(self):
+        self.assertTrue(mm.nash_equilibrium(self.m2)==False)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(BasicMatrixGame)
 unittest.TextTestRunner(verbosity=2).run(suite)
