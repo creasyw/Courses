3e9b09eca36914bfc8327a06ca1a25117d9bc2b0
creasyw <creasywuqiong@gmail.com>
2014-04-13 23:30:49 -0500

    compact representation

--- courses/machine_learning/ex3/lrCostFunction.m
+++ courses/machine_learning/ex3/lrCostFunction.m
@@ -37,11 +37,15 @@ grad = zeros(size(theta));
 %
 
 
-J = sum(-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta)))/m + lambda*sum(theta(2:length(theta)).^2)/(2*m);
 
-grad(1) = sum((sigmoid(X*theta)-y).*X(:,1))/m;
-grad(2:length(grad)) = sum((sigmoid(X*theta)-y).*X(:,2:size(X,2)))/m;
-grad(2:length(grad)) += lambda*theta(2:length(theta))/m;
+prediction = sigmoid(X*theta);
+
+J = 1 / m * sum(-y .* log(prediction) - (1 - y) .* log(1 - prediction)) + (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
+grad = 1 / m * (X' * (prediction - y)) + (lambda / m) .* [0;theta(2:end)];
+
+
+
+
 
 
 % =============================================================
