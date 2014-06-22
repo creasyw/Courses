faf3a5826f6ed30e1c95069ce429756cd972c173
creasyw <creasywuqiong@gmail.com>
2014-06-22 16:08:18 -0500

    add test suite for phase 2

--- courses/principles_of_computing/pj2.py
+++ courses/principles_of_computing/pj2.py
@@ -123,7 +123,7 @@ def simulate_clicker(build_info, duration, strategy):
     while duration >= 0:
         item = strategy(st.cur_cookies, st.cps, duration, local_info)
         cost = local_info.get_cost(item)
-        time = st.time_until(cost-st.cur_cookies)
+        time = st.time_until(cost)
         if time > duration:
             break
         else:
@@ -221,12 +221,28 @@ def phase_one_tests():
     test.buy_item("item", 5, 1)
     TESTSUITE.run_test("%0.1f, %0.1f" %(test.get_cookies(), test.get_cps()), "0.0, 2.0","Test 14: buy_item() should do nothing if cost > current cookies")
 
+def phase_two_tests():
+    try:
+        test = simulate_clicker(provided.BuildInfo(), 5.0, strategy_cursor)
+    except KeyError:
+        print("You are trying to call some of BuildInfo methods inside of simulate_clicker with None as item")
+        return
+    TESTSUITE.run_test(str(test.get_history()), "[(0.0, None, 0.0, 0.0)]", "Test 15: strategy_none should not change history")
+    TESTSUITE.run_test(str(test.get_cookies()), "5.0", "Test 16: strategy_none should generate t cookies in t time")
+
+    test = simulate_clicker(provided.BuildInfo(), SIM_TIME, strategy_cursor)
+    TESTSUITE.run_test("%11.1f, %11.1f, %0.1f" %(test.get_time(),test.get_cookies(), test.get_cps()),\
+                       "10000000000.0, 6965195661.5, 16.1", "Test 17: strategy_cursor doesn't work as expected")
+
+    test = simulate_clicker(provided.BuildInfo(), 15.0, strategy_cursor)
+    TESTSUITE.run_test("%0.1f, %0.1f, %0.1f" %(test.get_time(), test.get_cookies(), test.get_cps()),\
+                       "15.0, 0.0, 1.1", "Test 18: simulate_clicker should buy item if sim time == item cost")
+
+
 def run_tests(simulate_clicker=None, strategy_cursor=None, strategy_cheap=None, strategy_expensive=None, strategy_best=None):
     phase_one_tests()
-    if simulate_clicker != None:
-        phase_two_tests(simulate_clicker,  strategy_cursor)
-        if strategy_cheap != None:
-            phase_three_tests(simulate_clicker, strategy_cursor, strategy_cheap, strategy_expensive, strategy_best)
+    phase_two_tests()
+    #phase_three_tests(simulate_clicker, strategy_cursor, strategy_cheap, strategy_expensive, strategy_best)
     TESTSUITE.report_results()
 
 run_tests()
