1be28da7e98d0e75fe1cc62ccc061d355dffeb8a
creasyw <creasywuqiong@gmail.com>
2014-06-24 14:40:22 -0500

    basic version

---
+++ courses/principles_of_computing/pj3.py
@@ -0,0 +1,26 @@
+"""
+Monte Carlo Tic-Tac-Toe Player
+"""
+
+import random
+import poc_ttt_gui
+import poc_ttt_provided as provided
+
+# Constants for Monte Carlo simulator
+# Change as desired
+NTRIALS = 1    # Number of trials to run
+MCMATCH = 1.0  # Score for squares played by the machine player
+MCOTHER = 1.0  # Score for squares played by the other player
+    
+# Add your functions here.
+
+
+
+# Test game with the console or the GUI.
+# Uncomment whichever you prefer.
+# Both should be commented out when you submit for
+# testing to save time.
+
+# provided.play_game(mc_move, NTRIALS, False)        
+# poc_ttt_gui.run_gui(3, provided.PLAYERX, mc_move, NTRIALS, False)
+
