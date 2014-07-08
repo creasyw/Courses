563c6fe780f3a4a49c0faee6457cabe2f209f027
creasyw <creasywuqiong@gmail.com>
2014-07-08 16:53:35 -0500

    add function about guards, and pattern matching both for variables and function

--- courses/learn_haskell/ch4.hs
+++ courses/learn_haskell/ch4.hs
@@ -8,3 +8,19 @@ factorial x
 
 length' [] = 0
 length' (x:xs) = 1 + length' xs
+
+max' a b | a > b = a | otherwise = b
+
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi<=fat = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
+        (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi weight height | (weight, height) <- xs]
+  where bmi weight height = weight / height ^ 2
