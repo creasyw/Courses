43762e0493a6bf4b0282a398a2ca566fde49dc52
creasyw <creasywuqiong@gmail.com>
2014-07-07 21:59:25 -0500

    add signature and change condition

--- courses/learn_haskell/ch4.hs
+++ courses/learn_haskell/ch4.hs
@@ -1,8 +1,9 @@
 -- patter matching could be achieve either from repeating the name of
 -- the function (in the exactly the same form) or using guards
 
+factorial :: Integral a => a -> a
 factorial x
-  | x == 1 = 1
+  | x <= 1 = 1
   | otherwise = x * factorial (x-1)
 
 length' [] = 0
