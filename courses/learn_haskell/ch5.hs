1f7b79ef2ea8dc57f2a55b71563286c2182484f7
creasyw <creasywuqiong@gmail.com>
2014-07-09 21:31:20 -0500

    Add function only reverse the sequence of words

--- courses/learn_haskell/ch5.hs
+++ courses/learn_haskell/ch5.hs
@@ -4,3 +4,17 @@ maximum' (x:xs) = max x (maximum' xs)
 
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
+
+-- TODO: still not sure how to write local helper function
+-- This is a LISP-style tail-recursive function used in the following
+splitOn id str acc result
+  | length str == 0 = result ++ [acc]
+  | head str == id = splitOn id (tail str) "" (result ++ [acc])
+  | otherwise = splitOn id (tail str) (acc ++ [head str]) result
+
+assemble lst str
+  | length lst == 0 = str
+  | length str == 0 = assemble (tail lst) (head lst)
+  | otherwise = assemble (tail lst) (str++" "++(head lst))
+
+reverseWordSeq str = assemble (reverse $ splitOn ' ' str "" []) ""
