0800845f8cfb2197fb81a6677fe4d33a6bccf637
creasyw <creasywuqiong@gmail.com>
2014-07-14 15:02:39 -0500

    basic implementation of stack

---
+++ courses/principles_of_computing/pj5/stack.py
@@ -0,0 +1,84 @@
+"""
+Stack class
+"""
+
+class Stack:
+    """
+    A simple implementation of a FILO stack.
+    """
+
+    def __init__(self):
+        """ 
+        Initialize the stack.
+        """
+        self._items = []
+
+    def __len__(self):
+        """
+        Return number of items in the stack.
+        """
+        return len(self._items)
+
+    def __str__(self):
+        """
+        Returns a string representation of the stack.
+        """
+        return str(self._items)
+
+    def push(self, item):
+        """
+        Push item onto the stack.
+        """        
+        self._items.append(item)
+
+    def pop(self):
+        """
+        Pop an item off of the stack
+        """
+        return self._items.pop(-1)
+
+    def clear(self):
+        """
+        Remove all items from the stack.
+        """
+        self._items = []
+
+############################
+# test code for the stack
+
+my_stack = Stack()
+my_stack.push(72)
+my_stack.push(59)
+my_stack.push(33)
+my_stack.pop()
+my_stack.push(77)
+my_stack.push(13)
+my_stack.push(22)
+my_stack.push(45)
+my_stack.pop()
+my_stack.pop()
+my_stack.push(22)
+my_stack.push(72)
+my_stack.pop()
+my_stack.push(90)
+my_stack.push(67)
+while len(my_stack) > 4:
+    my_stack.pop()
+my_stack.push(32)
+my_stack.push(14)
+my_stack.pop()
+my_stack.push(65)
+my_stack.push(87)
+my_stack.pop()
+my_stack.pop()
+my_stack.push(34)
+my_stack.push(38)
+my_stack.push(29)
+my_stack.push(87)
+my_stack.pop()
+my_stack.pop()
+my_stack.pop()
+my_stack.pop()
+my_stack.pop()
+my_stack.pop()
+print my_stack.pop()
