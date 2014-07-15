9df44485344c82cc2f073caae0dfd56f6945a9cc
creasyw <creasywuqiong@gmail.com>
2014-07-14 22:43:01 -0500

    another curry

--- courses/learn_haskell/ch6.hs
+++ courses/learn_haskell/ch6.hs
@@ -5,3 +5,6 @@ compareWithHundred = compare 100
 -- curry as infix
 dividedByTen = (/10)
 dividTen = (10/)
+
+-- the missing variable is in the middle of original expression
+isUpperCase = (`elem` ['A'..'Z'])
