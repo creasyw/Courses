2203d6525c63c6997994afb7ac3093cfc8ac1966
creasyw <creasywuqiong@gmail.com>
2014-07-15 15:19:34 -0500

    implement zipWith using higher order function

--- courses/learn_haskell/ch6.hs
+++ courses/learn_haskell/ch6.hs
@@ -8,3 +8,7 @@ dividTen = (10/)
 
 -- the missing variable is in the middle of original expression
 isUpperCase = (`elem` ['A'..'Z'])
+
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
