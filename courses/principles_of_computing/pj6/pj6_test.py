01bfefb5e64ea8b454a1254948bf46a5870ac93f
creasyw <creasywuqiong@gmail.com>
2014-07-19 14:15:35 -0500

    add testing for merge_sort

--- courses/principles_of_computing/pj6/pj6_test.py
+++ courses/principles_of_computing/pj6/pj6_test.py
@@ -32,6 +32,17 @@ def test_merge():
     suite.run_test(mycode.merge([1,2,3], [0,4,5]), [0,1,2,3,4,5], "Test 4")
     suite.report_results()
 
+def test_merge_sort():
+    suite = poc_simpletest.TestSuite()
+    print("test_merge_sorty():"),
+    suite.run_test(mycode.merge_sort([]), [], "Test 0")
+    suite.run_test(mycode.merge_sort([1]), [1], "Test 1")
+    suite.run_test(mycode.merge_sort([1,2,3]), [1,2,3], "Test 2")
+    suite.run_test(mycode.merge_sort([1,4,2,3]), [1,2,3,4], "Test 3")
+    suite.run_test(mycode.merge_sort([38,27,43,3,9,82,10]), [3,9,10,27,38,43,82], "Test 4")
+    suite.report_results()
+
 test_remove_duplicates()
 test_intersect()
 test_merge()
+test_merge_sort()
