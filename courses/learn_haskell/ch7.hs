e50c41cf0108dc3f62a2597bd5c545a3b68b8a31
creasyw <creasywuqiong@gmail.com>
2014-07-25 21:16:32 -0500

    add examples for build-in funcs

--- courses/learn_haskell/ch7.hs
+++ courses/learn_haskell/ch7.hs
@@ -2,3 +2,15 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+tryout_buildin = do
+  putStrLn $ intercalate " " ["hey","there","guys"]
+  putStrLn $ intersperse '.' "DOG"
+  -- transpose uses the iteration to reverse the row and col of the
+  -- list, so it doesn't mind if the list is a 'real' matrix
+  putStrLn $ show $ transpose [[1,2,3],[4,5,6],[7,8,9]]
+  putStrLn $ show $ sum $ takeWhile (<10000) $ map (^3) [1..]
+
+-- find how many times each element appears in the list
+countDup :: (Eq a0, Ord a0) => [a0] -> [(a0, Int)]
+countDup = map (\l@(x:xs) -> (x, length l)) . group . sort
