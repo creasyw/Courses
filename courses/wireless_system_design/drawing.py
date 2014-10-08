277a8b25cda627d0c14c88cb28fcceb9a457a094
creasyw <creasywuqiong@gmail.com>
2014-10-07 20:23:01 -0500

    complete label and axis setting

--- courses/wireless_system_design/drawing.py
+++ courses/wireless_system_design/drawing.py
@@ -6,7 +6,10 @@ data = np.genfromtxt('score.csv')
 plt.hist(data, bins=50)
 ax = plt.gca()
 plt.xlim(ax.get_xlim()[::-1])
-plt.show()
+plt.ylim(0, int(max(ax.get_ylim()))+1)
+plt.xlabel("Grade", fontsize=15)
+plt.ylabel("Number of Students", fontsize=15)
+plt.savefig("score_distr.pdf", format='pdf')
 
 
 
