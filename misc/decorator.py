926de6fd354e1594b7846b05b538bac7d828a7b8
creasy <creasywuqiong@gmail.com>
2015-05-10 11:04:11 -0700

    test for decorator

---
+++ misc/decorator.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+def tags(tag_name):
+    def tags_decorator(func):
+        @wraps(func)
+        def func_wrapper(name):
+            return "<{0}>{1}</{0}>".format(tag_name, func(name))
+        return func_wrapper
+    return tags_decorator
+
+@tags("p")
+def get_text(name):
+    """returns some text"""
+    return "Hello "+name
+
+print get_text.__name__ # get_text
+print get_text.__doc__ # returns some text
+print get_text.__module__ # __main__
