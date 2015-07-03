8e88ff7386ae889a22c6c97ca951f00cdb9d9f59
Qiong Wu <creasywuqiong@gmail.com>
2015-07-03 17:18:44 -0500

    delete original example

--- data_wrangling/lesson1/parse_json.py
+++ data_wrangling/lesson1/parse_json.py
@@ -14,7 +14,7 @@ query_type = {  "simple": {},
 def query_site(url, params, uid="", fmt="json"):
     params["fmt"] = fmt
     r = requests.get(url + uid, params=params)
-    print "requesting", r.url
+    #print "requesting", r.url
 
     if r.status_code == requests.codes.ok:
         return r.json()
@@ -35,23 +35,6 @@ def pretty_print(data, indent=4):
 
 
 def main():
-    results = query_by_name(ARTIST_URL, query_type["simple"], "Nirvana")
-    pretty_print(results)
-
-    artist_id = results["artists"][1]["id"]
-    print "\nARTIST:"
-    pretty_print(results["artists"][1])
-
-    artist_data = query_site(ARTIST_URL, query_type["releases"], artist_id)
-    releases = artist_data["releases"]
-    print "\nONE RELEASE:"
-    pretty_print(releases[0], indent=2)
-    release_titles = [r["title"] for r in releases]
-
-    print "\nALL TITLES:"
-    for t in release_titles:
-        print t
-
 
 if __name__ == '__main__':
     main()
