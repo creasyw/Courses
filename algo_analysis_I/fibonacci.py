5b6fb93072c7d434a39d21e84fda38eceb1463e9
Qiong Wu <creasywuqiong@gmail.com>
2015-08-12 23:20:35 -0700

    style compliance

--- algo_analysis_I/fibonacci.py
+++ algo_analysis_I/fibonacci.py
@@ -1,9 +1,9 @@
 def fib(n):
-    temp = [1,1,2]
-    if n<3:
+    temp = [1, 1, 2]
+    if n < 3:
         return temp[n]
     else:
-        for i in range(3,n):
-            index = i%3
-            temp[index] = temp[(i+1)%3]+temp[(i+2)%3]
+        for i in range(3, n):
+            index = i % 3
+            temp[index] = temp[(i + 1) % 3] + temp[(i + 2) % 3]
         return temp[index]
