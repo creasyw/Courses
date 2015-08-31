8f709dcaeb0c72229deb4793fc4d0ec5f119e746
Qiong Wu <creasywuqiong@gmail.com>
2015-08-30 23:27:26 -0700

    style compliance

--- algo_analysis_II/hw5_tsp/tsp.large.py
+++ algo_analysis_II/hw5_tsp/tsp.large.py
@@ -7,15 +7,18 @@ import gc
 
 finder = re.compile("-?\d+\.?\d*")
 
-def gen_name(lst):
-    return reduce(lambda i, j: i+str(j), sorted(lst), '')
 
-def post_processing (half_way, old, graph, dict):
+def gen_name(lst):
+    return reduce(lambda i, j: i + str(j), sorted(lst), '')
+
+
+def post_processing(half_way, old, graph, dict):
     bucket = {}
     for i in half_way:
         this = [0]
-        match = gen_name(list( (set(range(len(graph))) - set(half_way[i].keys())) | set(this)))
-        if len(graph)%2 == 1:
+        match = gen_name(list((set(range(len(graph))) - set(half_way[i].keys())
+                               ) | set(this)))
+        if len(graph) % 2 == 1:
             distance = min(half_way[i][m] + half_way[match][n] + \
                     dict[m, n] for m in half_way[i] for n in half_way[match])
         else:
@@ -30,18 +33,18 @@ def tsp(graph):
     # precompute the distance between nodes
     dict = np.zeros((len(graph), len(graph)))
     for i in range(len(graph)):
-        for j in range(i+1):
+        for j in range(i + 1):
             dict[i][j] = dict[j][i] = \
                     sqrt((graph[i][0]-graph[j][0])**2 + \
                     (graph[i][1]-graph[j][1])**2)
-    maximum = np.max(dict)*len(graph)*0.5
+    maximum = np.max(dict) * len(graph) * 0.5
     # initialize the starting array
     old = {}
-    old['0'] = {0:0}
+    old['0'] = {0: 0}
     m = 2
 
     while True:
-        sys.stdout.write("\rNow computing length: %s"%(m))
+        sys.stdout.write("\rNow computing length: %s" % (m))
         sys.stdout.flush()
         gc.collect()
         #current = filter(lambda x: 0 in x, combinations(range(len(graph)), m))
@@ -55,14 +58,17 @@ def tsp(graph):
                 temp = list(s)
                 temp.remove(j)
                 old_name = gen_name(temp)
-                current_dict[cur_name][j] = min(old[old_name][k]+dict[k,j] for k in temp if k!=j and k in old[old_name])
+                current_dict[cur_name][j] = min(
+                    old[old_name][k] + dict[k, j]
+                    for k in temp if k != j and k in old[old_name])
         m += 1
-        if m >= len(graph)/2+2: break
+        if m >= len(graph) / 2 + 2: break
         old = {}
         for i in current_dict:
             old[i] = {}
-            temp = np.array(sorted(current_dict[i].iteritems(), key=operator.itemgetter(1))[:4])
-            for j in temp[:,0]:
+            temp = np.array(sorted(current_dict[i].iteritems(),
+                                   key=operator.itemgetter(1))[:4])
+            for j in temp[:, 0]:
                 old[i][j] = current_dict[i][j]
 
     # To use dynamic programming exploring TSP, as it calculate smallest distance for N/2 vertex,
@@ -71,17 +77,20 @@ def tsp(graph):
     print "\n\n"
     return post_processing(current_dict, old, graph, dict)
 
+
 def main():
     import sys
-    assert len(sys.argv)==2, "The proper input format is: ~$ python SCRIPT.py data_file start_node"
+    assert len(
+        sys.argv) == 2, "The proper input format is: ~$ python SCRIPT.py data_file start_node"
     filename = sys.argv[1]
     data = []
     with open(os.path.join(os.path.dirname(__file__), filename)) as datafile:
-            num = int(datafile.readline())
-            for row in datafile:
-                data.append([float(k) for k in finder.findall(row)])
-    
+        num = int(datafile.readline())
+        for row in datafile:
+            data.append([float(k) for k in finder.findall(row)])
+
     print tsp(np.array(data))
+
 
 if __name__ == "__main__":
     main()
--- algo_analysis_II/hw5_tsp/tsp.py
+++ algo_analysis_II/hw5_tsp/tsp.py
@@ -6,16 +6,19 @@ import operator
 
 finder = re.compile("-?\d+\.?\d*")
 
-def gen_name(lst):
-    return reduce(lambda i, j: i+str(j), sorted(lst), '')
 
-def post_processing (half_way, old, graph, dict):
+def gen_name(lst):
+    return reduce(lambda i, j: i + str(j), sorted(lst), '')
+
+
+def post_processing(half_way, old, graph, dict):
     candidates = half_way.copy()
     bucket = {}
     for i in candidates:
         this = [0]
-        match = gen_name(list( (set(range(len(graph))) - set(half_way[i].keys())) | set(this)))
-        if len(graph)%2 == 1:
+        match = gen_name(list((set(range(len(graph))) - set(half_way[i].keys())
+                               ) | set(this)))
+        if len(graph) % 2 == 1:
             distance = min(candidates[i][m] + candidates[match][n] + \
                     dict[m, n] for m in candidates[i] for n in candidates[match])
         else:
@@ -26,18 +29,17 @@ def post_processing (half_way, old, graph, dict):
     return min(bucket)
 
 
-
 def tsp(graph):
     # precompute the distance between nodes
     dict = np.zeros((len(graph), len(graph)))
     for i in range(len(graph)):
-        for j in range(i+1):
+        for j in range(i + 1):
             dict[i][j] = dict[j][i] = \
                     sqrt((graph[i][0]-graph[j][0])**2 + \
                     (graph[i][1]-graph[j][1])**2)
     # initialize the starting array
     old = {}
-    old['0'] = {0:0}
+    old['0'] = {0: 0}
     m = 2
 
     while True:
@@ -51,29 +53,31 @@ def tsp(graph):
                 temp = list(s)
                 temp.remove(j)
                 old_name = gen_name(temp)
-                current_dict[cur_name][j] = min(old[old_name][k]+dict[k,j] for k in temp if k!=j)
+                current_dict[cur_name][j] = min(old[old_name][k] + dict[k, j]
+                                                for k in temp if k != j)
             current_dict[cur_name][0] = float('inf')
         m += 1
-        if m >= len(graph)/2+2: break
+        if m >= len(graph) / 2 + 2: break
         old = current_dict
     # To use dynamic programming exploring TSP, as it calculate smallest distance for N/2 vertex,
     # actually, the problem has already be solved. Because the other half also has already in the
     # dataset.
     return post_processing(current_dict, old, graph, dict)
 
+
 def main():
     import sys
-    assert len(sys.argv)==2, "The proper input format is: ~$ python SCRIPT.py data_file start_node"
+    assert len(
+        sys.argv) == 2, "The proper input format is: ~$ python SCRIPT.py data_file start_node"
     filename = sys.argv[1]
     data = []
     with open(os.path.join(os.path.dirname(__file__), filename)) as datafile:
-            num = int(datafile.readline())
-            for row in datafile:
-                data.append([float(k) for k in finder.findall(row)])
-    
+        num = int(datafile.readline())
+        for row in datafile:
+            data.append([float(k) for k in finder.findall(row)])
+
     print tsp(np.array(data))
+
 
 if __name__ == "__main__":
     main()
-
-
