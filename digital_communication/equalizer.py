ec9de2e886b9b0396cae36f509c811a3b41b2d89
Qiong Wu <creasywuqiong@gmail.com>
2015-09-05 23:29:47 -0700

    Fix style

--- digital_communication/equalizer.py
+++ digital_communication/equalizer.py
@@ -8,6 +8,7 @@ from itertools import product
 tap1 = np.array([0.227, 0.45, 0.7, 0.45, 0.227])
 tap2 = np.array([0.41, 0.815, 0.41])
 
+
 def awgn(nsample, nvar):
     """
     Generate noise with standard normal distribution.
@@ -16,81 +17,93 @@ def awgn(nsample, nvar):
     """
     return np.random.normal(loc=0, scale=sqrt(nvar), size=nsample)
 
+
 def binary_pam(nsample):
     """ Generate PAM symbols with equal probability. """
-    return np.array(map(lambda x: -1 if x==0 else x, np.random.randint(2, size=nsample)))
+    return np.array(map(lambda x: -1 if x == 0 else x,
+                        np.random.randint(2,
+                                          size=nsample)))
+
 
 def channel(signal, tap, snr):
     """ Construct the effect of ISI and AWGN. """
     L = len(tap)
-    output = awgn(len(signal), 10**(-snr/10.))
+    output = awgn(len(signal), 10**(-snr / 10.))
     for n in range(len(signal)):
-        if n < L-1:
-            current_in_loop = np.array([0]*(L-n-1)+list(signal[:(n+1)]))
+        if n < L - 1:
+            current_in_loop = np.array([0] * (L - n - 1) + list(signal[:(n + 1
+                                                                         )]))
         else:
-            current_in_loop = (signal[:(n+1)])[-L:]
-        output[n] += sum(current_in_loop*(tap[::-1]))
+            current_in_loop = (signal[:(n + 1)])[-L:]
+        output[n] += sum(current_in_loop * (tap[::-1]))
     return output
+
 
 def zero_forcing_coeff(tap, nzf):
     """ Calculate the coefficients for zero forcing equalizer """
-    index = np.zeros((nzf+len(tap)-1, nzf), dtype=float)
-    q = np.zeros(nzf+len(tap)-1, dtype=float)
-    q[nzf/2] = 1
+    index = np.zeros((nzf + len(tap) - 1, nzf), dtype=float)
+    q = np.zeros(nzf + len(tap) - 1, dtype=float)
+    q[nzf / 2] = 1
     fz = tap
     # Construct the index array for coefficients
     for n in range(len(index)):
-        for j in range(n, n-3, -1):
-            if j<0 or j>=nzf: continue
-            index[n, j] = fz[n-j]
+        for j in range(n, n - 3, -1):
+            if j < 0 or j >= nzf: continue
+            index[n, j] = fz[n - j]
     # Solve the linear function using least square linear solver
-    coeff,_,_,_ =  np.linalg.lstsq(index, q)
+    coeff, _, _, _ = np.linalg.lstsq(index, q)
     return coeff
+
 
 def lmmse_coeff(fz, nzf, snr):
     """ Calculate the coefficients for Linear Minimum MSE equalizer """
     r = np.zeros((nzf, nzf), dtype=float)
     for l in range(nzf):
         # adding noise to the diagnal elements
-        r[l, l] = 10**(-snr/10.)
+        r[l, l] = 10**(-snr / 10.)
         for j in range(nzf):
             for n in range(len(fz)):
-                if 0<n+l-j<len(fz): r[l,j] += np.conjugate(fz[n])*fz[n+l-j]
+                if 0 < n + l - j < len(fz):
+                    r[l, j] += np.conjugate(fz[n]) * fz[n + l - j]
     # Construct the upsilon and flip it
     upsilon = np.zeros(nzf, dtype=float)
-    zero = nzf/2
-    upsilon[zero-len(fz)+1:zero+1] = np.conjugate(fz)[::-1]
-    return np.array([sum(k) for k in np.linalg.inv(r)*upsilon])
+    zero = nzf / 2
+    upsilon[zero - len(fz) + 1:zero + 1] = np.conjugate(fz)[::-1]
+    return np.array([sum(k) for k in np.linalg.inv(r) * upsilon])
+
 
 def dfe_coeff(tap, k1, k2, snr):
     """ Calculate the coefficients for Decision Feedback equalizer """
     # For feed-forward loop
     r = np.zeros((k1, k1), dtype=float)
     for l in range(k1):
-        r[l, l] = 10**(-snr/10.)
+        r[l, l] = 10**(-snr / 10.)
         for j in range(k1):
             for n in range(len(tap)):
-                if 0<n+l-j<len(tap): r[l,j] += np.conjugate(tap[n])*tap[n+l-j]
+                if 0 < n + l - j < len(tap):
+                    r[l, j] += np.conjugate(tap[n]) * tap[n + l - j]
     upsilon = np.zeros(k1, dtype=float)
-    zero = k1/2
-    upsilon[zero-len(tap)+1:zero+1] = np.conjugate(tap)[::-1]
-    cj = [sum(k) for k in np.linalg.inv(r)*upsilon]
+    zero = k1 / 2
+    upsilon[zero - len(tap) + 1:zero + 1] = np.conjugate(tap)[::-1]
+    cj = [sum(k) for k in np.linalg.inv(r) * upsilon]
     # For feed-back loop
     temp = cj[::-1]
     ck = np.zeros(k2, dtype=float)
     for i in range(k2):
         for j in range(k1):
-            if 0<=i+j<len(tap):
-                ck[i] -= temp[j]*tap[i+j]
+            if 0 <= i + j < len(tap):
+                ck[i] -= temp[j] * tap[i + j]
     return cj, ck
+
 
 def dfe_estimate(vn, cj, ck):
     """ Combine the forward and backward estimates together. """
     decision = np.zeros(len(vn), dtype=float)
     forward = np.convolve(vn, cj, mode='same')
-    fdec = np.array(map(lambda x: -1 if x<0 else 1, forward))
+    fdec = np.array(map(lambda x: -1 if x < 0 else 1, forward))
     backward = np.convolve(fdec, ck, mode='same')
-    return np.array(map(lambda x: -1 if x<0 else 1, backward+forward))
+    return np.array(map(lambda x: -1 if x < 0 else 1, backward + forward))
+
 
 def viterbi_function(signal, taps, k, values, known):
     """
@@ -104,37 +117,40 @@ def viterbi_function(signal, taps, k, values, known):
     result = 0
     minimum = float("inf")
     for unknown in product(values, repeat=len(taps)):
-        current = known+list(unknown)
-        unknown  = np.array(unknown)
+        current = known + list(unknown)
+        unknown = np.array(unknown)
         temp = unknown[0]
-        if k+L > len(signal):
+        if k + L > len(signal):
             # im not sure if the unknow*tap should be added here, though it doesnt matter...
             summation = 0
         else:
-            summation = (signal[k+L-1] - sum(unknown*(taps[::-1])))**2
-        for i in range(L+k-1):
+            summation = (signal[k + L - 1] - sum(unknown * (taps[::-1])))**2
+        for i in range(L + k - 1):
             # won't look beyond the scope of signal
             if i >= len(signal): continue
-            current_in_loop = current[:(i+1)]
+            current_in_loop = current[:(i + 1)]
             # extend zeros before the unknow values
             if len(current_in_loop) < L:
-                current_in_loop = np.array([0]*(L-len(current_in_loop))+current_in_loop)
+                current_in_loop = np.array([0] * (L - len(current_in_loop)) +
+                                           current_in_loop)
             else:
                 current_in_loop = np.array(current_in_loop[-L:])
-            summation += (signal[i] - sum(current_in_loop*(taps[::-1])))**2
+            summation += (signal[i] - sum(current_in_loop * (taps[::-1])))**2
         if summation < minimum:
             result = temp
             minimum = summation
 
     return result
 
+
 def viterbi(signal, taps):
     result = []
-    values = [-1,1]
+    values = [-1, 1]
     for i in range(len(signal)):
         result.append(viterbi_function(signal, taps, i, values, result))
 
     return result
+
 
 def equalizer(nchannel, nzf, snrlst, nsample, eqtype):
     """
@@ -151,7 +167,7 @@ def equalizer(nchannel, nzf, snrlst, nsample, eqtype):
         tap = tap2
     else:
         raise ValueError("The channel shoudl be either 1 or 2!")
-    
+
     ser = np.zeros(len(snrlst), dtype=float)
     for i in range(len(snrlst)):
         snr = snrlst[i]
@@ -166,25 +182,31 @@ def equalizer(nchannel, nzf, snrlst, nsample, eqtype):
         elif eqtype == "viterbi":
             estimate = viterbi(vn, tap)
         else:
-            raise ValueError("The type of equalizer should be 'zf' (zero-forcing) or\
+            raise ValueError(
+                "The type of equalizer should be 'zf' (zero-forcing) or\
                     lmmse (Linear Minimumu MSE)!")
-        
+
         if eqtype != "dfe" and eqtype != "viterbi":
-            estimate = np.array(map(lambda x: -1 if x<0 else 1, np.convolve(vn, coeff, mode='same')))
+            estimate = np.array(map(lambda x: -1 if x < 0 else 1,
+                                    np.convolve(vn,
+                                                coeff,
+                                                mode='same')))
         elif eqtype == "dfe":
             estimate = dfe_estimate(vn, cj, ck)
-        
+
         # Compare teh estimates with the original sequence
-        ser[i] = sum(1 for i in range(nsample) if samples[i]!=estimate[i])/float(nsample)
+        ser[i] = sum(1 for i in range(nsample)
+                     if samples[i] != estimate[i]) / float(nsample)
     return ser
+
 
 def plotzf():
     """ Test Simulation 1-1 
         Plot simulatied zero forcing equalizer with different taps"""
     nsample = 10**5
-    snr = range(0,19)
-    taps = [13,23,33]
-    
+    snr = range(0, 19)
+    taps = [13, 23, 33]
+
     fig1 = plt.figure()
     plt.semilogy(snr, equalizer(2, 13, snr, nsample, 'zfir'),\
             snr, equalizer(2, 23, snr, nsample, 'zfir'), "--", \
@@ -196,20 +218,22 @@ def plotzf():
     plt.grid(True, which='both')
     plt.show()
 
+
 def plotzfe():
     """ Test Simulation 1-2
         Plot theoretical vs. simulated results for zero forcing equalizer."""
     nsample = 10**5
-    snrlst = range(0,19)
+    snrlst = range(0, 19)
 
     # Calculate the theoretical values
     pe = []
     coeff = zero_forcing_coeff(tap2, 41)
     for snr in snrlst:
-        delta_square = 10**(-snr/10.)*sum(coeff**2)
-        pe.append(1-norm.cdf(sqrt((1-0.41)**2/delta_square)))
-    
-    plt.semilogy(snrlst,pe,snrlst,equalizer(2,41,snrlst,nsample,'zfir'),"-.")
+        delta_square = 10**(-snr / 10.) * sum(coeff**2)
+        pe.append(1 - norm.cdf(sqrt((1 - 0.41)**2 / delta_square)))
+
+    plt.semilogy(snrlst, pe, snrlst, equalizer(2, 41, snrlst, nsample, 'zfir'),
+                 "-.")
     plt.legend(("Theoretical curve", "41 taps simulation"), loc='lower left')
     plt.title("Theoretical vs. Simulated performances")
     plt.xlabel("SNR (dB)")
@@ -217,17 +241,19 @@ def plotzfe():
     plt.grid(True, which='both')
     plt.show()
 
+
 def plotlmmse():
     nsample = 10**5
-    snrlst = range(0,19)
-    
+    snrlst = range(0, 19)
+
     plt.subplot(211)
     pe = []
     for snr in snrlst:
         coeff = lmmse_coeff(tap1, 41, snr)
-        delta_square = 10**(-snr/10.)*sum(coeff**2)
-        pe.append(1-norm.cdf(sqrt((1-0.7)**2/delta_square)))
-    plt.semilogy(snrlst,pe,snrlst,equalizer(2,41,snrlst,nsample,'lmmse'),"-.")
+        delta_square = 10**(-snr / 10.) * sum(coeff**2)
+        pe.append(1 - norm.cdf(sqrt((1 - 0.7)**2 / delta_square)))
+    plt.semilogy(snrlst, pe, snrlst, equalizer(2, 41, snrlst, nsample,
+                                               'lmmse'), "-.")
     plt.legend(("Theoretical curve", "Simulated curve"), loc='lower left')
     plt.title("Theoretical vs. Simulated performances for Channel 1")
     plt.xlabel("SNR (dB)")
@@ -238,9 +264,10 @@ def plotlmmse():
     pe = []
     for snr in snrlst:
         coeff = lmmse_coeff(tap2, 41, snr)
-        delta_square = 10**(-snr/10.)*sum(coeff**2)
-        pe.append(1-norm.cdf(sqrt((1-0.41)**2/delta_square)))
-    plt.semilogy(snrlst,pe,snrlst, equalizer(2, 41, snrlst, nsample, 'lmmse'), "-.")
+        delta_square = 10**(-snr / 10.) * sum(coeff**2)
+        pe.append(1 - norm.cdf(sqrt((1 - 0.41)**2 / delta_square)))
+    plt.semilogy(snrlst, pe, snrlst, equalizer(2, 41, snrlst, nsample,
+                                               'lmmse'), "-.")
     plt.legend(("Theoretical curve", "Simulated curve"), loc='lower left')
     plt.title("Theoretical vs. Simulated performances for Channel 2")
     plt.xlabel("SNR (dB)")
@@ -248,12 +275,13 @@ def plotlmmse():
     plt.grid(True, which='both')
     plt.show()
 
+
 def plotdfe1():
     nsample = 10**5
-    snrlst = range(0,19)
+    snrlst = range(0, 19)
     nzf = 41
     tap = tap2
-    
+
     plt.semilogy(snrlst, equalizer(2, 41, snrlst, nsample, 'dfe'), \
             snrlst, equalizer(2, 61, snrlst, nsample, 'zfir'), "-.",\
             snrlst, equalizer(2, 41, snrlst, nsample, 'zfir'), "--")
@@ -264,20 +292,22 @@ def plotdfe1():
     plt.grid(True, which='both')
     plt.show()
 
+
 def plotdfe2():
     nsample = 10**5
-    snrlst = range(0,19)
+    snrlst = range(0, 19)
     nzf = 41
     tap = tap2
-    
+
     pe = []
     for snr in snrlst:
-        cj,_ = dfe_coeff(tap, nzf, 41, snr)
-        f = [0]*(len(cj)-len(tap)) + list(tap)[::-1]
-        jmin = 1-sum(np.array(f)*cj)
-        gamma = (1-jmin)/jmin
-        pe.append(1-norm.cdf(sqrt(gamma)))
-    plt.semilogy(snrlst,pe,snrlst, equalizer(2, 41, snrlst, nsample, 'dfe'), "-.")
+        cj, _ = dfe_coeff(tap, nzf, 41, snr)
+        f = [0] * (len(cj) - len(tap)) + list(tap)[::-1]
+        jmin = 1 - sum(np.array(f) * cj)
+        gamma = (1 - jmin) / jmin
+        pe.append(1 - norm.cdf(sqrt(gamma)))
+    plt.semilogy(snrlst, pe, snrlst, equalizer(2, 41, snrlst, nsample, 'dfe'),
+                 "-.")
     plt.legend(("Theoretical curve", "Simulated curve"), loc='lower left')
     plt.title("Theoretical vs. Simulated performances for Channel 1")
     plt.xlabel("SNR (dB)")
@@ -285,12 +315,13 @@ def plotdfe2():
     plt.grid(True, which='both')
     plt.show()
 
+
 def plotviterbi_1():
     nsample = 10**5
-    snrlst = range(0,19)
+    snrlst = range(0, 19)
     nzf = 41
     tap = tap2
-    
+
     plt.semilogy(snrlst,equalizer(2, 10, snrlst, nsample, 'viterbi'),\
             snrlst, equalizer(2, 41, snrlst, nsample, 'dfe'), "-.")
     plt.legend(("ML viterbi", "MLSE-MF"), loc='lower left')
@@ -300,12 +331,13 @@ def plotviterbi_1():
     plt.grid(True, which='both')
     plt.show()
 
+
 def plotviterbi_2():
     nsample = 10**5
-    snrlst = range(0,19)
+    snrlst = range(0, 19)
     nzf = 41
     tap = tap2
-    
+
     plt.semilogy(snrlst,equalizer(2, 10, snrlst, nsample, 'viterbi'),\
             snrlst, equalizer(2, 10, snrlst, nsample, 'viterbi'), "--")
     plt.legend(("Channel 1", "Channel 2"), loc='lower left')
@@ -316,7 +348,7 @@ def plotviterbi_2():
     plt.show()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     plotzf()
     plotzfe()
     plotlmmse()
--- digital_communication/hw4.py
+++ digital_communication/hw4.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 # calculate the norm distance between points
-norm = lambda m: (reduce(lambda acc, itr: acc+itr**2, m, 0))**0.5
+norm = lambda m: (reduce(lambda acc, itr: acc + itr**2, m, 0))**0.5
+
 
 def signal_generation(Es):
     """
@@ -13,51 +14,59 @@ def signal_generation(Es):
     Besides, the length of signal is also given and hard-coded.
     The input variable is the energy of the signal.
     """
-    size = 3*10**5
-    low = 1; high = 9
+    size = 3 * 10**5
+    low = 1
+    high = 9
 
     rint = np.random.randint(low, high, size)
     signal = np.zeros((size, 2))
     # Mapping, regardless of the grey coding
-    signal[:,0] = map(lambda m: (Es)**0.5*cos(2*pi*m/8), rint)
-    signal[:,1] = map(lambda m: (Es)**0.5*sin(2*pi*m/8), rint)
+    signal[:, 0] = map(lambda m: (Es)**0.5 * cos(2 * pi * m / 8), rint)
+    signal[:, 1] = map(lambda m: (Es)**0.5 * sin(2 * pi * m / 8), rint)
     return rint, signal
 
-def transfer_in_channel (signal, Es, snr):
+
+def transfer_in_channel(signal, Es, snr):
     """
     Simulate the effect that signal is transferring in the channel.
     Note that the input variable SNR value is in LINEAR unit.
     """
     # Generate Gaussian noise with certain constraints
-    awgn = np.random.normal(loc=0.0, scale=(Es/log(3,2)/snr/2)**0.5, size=len(signal))
+    awgn = np.random.normal(loc=0.0,
+                            scale=(Es / log(3, 2) / snr / 2)**0.5,
+                            size=len(signal))
     noise = (np.vstack((awgn, awgn))).T
-    return signal+noise
+    return signal + noise
 
-def detection (receive, Es):
+
+def detection(receive, Es):
     """ Detect the received signal to symbol with MAP detector."""
-    cd = range(1,9)
+    cd = range(1, 9)
     # Mapping the codewords to modulation plane
     mapping = (np.vstack((map(lambda m: (Es)**0.5*cos(2*pi*m/8), cd), \
             map(lambda m: (Es)**0.5*sin(2*pi*m/8), cd)))).T
-    
+
     decoded = []
     for i in receive:
-        codeword = 0; mindis = float('+inf')
+        codeword = 0
+        mindis = float('+inf')
         # Performing nearest neighbor detection
         for j in range(len(mapping)):
-            distance = norm(i-mapping[j])
+            distance = norm(i - mapping[j])
             if distance < mindis:
                 mindis = distance
                 # j is the index of mapping, the real codeword should be j+1
-                codeword = j+1
-        assert codeword!=0, "There is something wrong in the decoding process!"
+                codeword = j + 1
+        assert codeword != 0, "There is something wrong in the decoding process!"
         decoded.append(codeword)
     return decoded
 
+
 def performance(tx, rx):
     """ Calculate the symbol error rate. """
-    count = sum(1 for i in range(len(tx)) if tx[i]==rx[i])
-    return 1-float(count)/len(tx)
+    count = sum(1 for i in range(len(tx)) if tx[i] == rx[i])
+    return 1 - float(count) / len(tx)
+
 
 def plotting(snr, ser, ser_max, ser_min):
     fig = plt.figure()
@@ -70,30 +79,33 @@ def plotting(snr, ser, ser_max, ser_min):
     plt.title("8-PSK Communication System in AWGN")
     plt.show()
 
+
 def theoretical(Es, snr):
-    q_function = lambda x: 1-norm.cdf(x)
-    dmin = norm(np.array([(Es)**0.5*cos(2*pi/8), (Es)**0.5*sin(2*pi/8)]) - np.array([(Es)**0.5, 0]))
-    noise = Es/(10**(np.array(snr)/10.))
+    q_function = lambda x: 1 - norm.cdf(x)
+    dmin = norm(np.array([(Es)**0.5 * cos(2 * pi / 8), (Es)**0.5 * sin(
+        2 * pi / 8)]) - np.array([(Es)**0.5, 0]))
+    noise = Es / (10**(np.array(snr) / 10.))
     # choose the 3rd upper bound of Pe
-    ser_max = 7*q_function(dmin/((2*noise)**0.5))
-    ser_min = 2/7.*q_function(dmin/((2*noise)**0.5))
+    ser_max = 7 * q_function(dmin / ((2 * noise)**0.5))
+    ser_min = 2 / 7. * q_function(dmin / ((2 * noise)**0.5))
     return ser_max, ser_min
+
 
 def main():
     Es = 1
-    snr = range(0,19)
+    snr = range(0, 19)
     tx, signal = signal_generation(Es)
     result = []
     for i in snr:
         # snr input to transfer_in_channel should be LINEAR.
-        rx = transfer_in_channel(signal, Es, 10**(i/10.))
+        rx = transfer_in_channel(signal, Es, 10**(i / 10.))
         decoded = detection(rx, Es)
         ser = performance(tx, decoded)
-        print "SNR = %s -- SER = %s."%(i, ser)
+        print "SNR = %s -- SER = %s." % (i, ser)
         result.append(ser)
     ser_max, ser_min = theoretical(Es, snr)
     plotting(snr, result, ser_max, ser_min)
 
-if __name__=="__main__":
-    main()
 
+if __name__ == "__main__":
+    main()
