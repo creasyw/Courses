2886f5900ea58b711528a80c80598a2571ed8cef
Qiong Wu <creasywuqiong@gmail.com>
2015-09-06 23:27:25 -0700

    style compliance

--- algo_analysis_II/hw6_2sat/sat.py
+++ algo_analysis_II/hw6_2sat/sat.py
@@ -5,16 +5,17 @@ from math import log
 
 finder = re.compile("-?\d+")
 
+
 def preprocess(clause, num):
-    new_clause = np.zeros((num+1,2), dtype=int)
+    new_clause = np.zeros((num + 1, 2), dtype=int)
     count = 0
     for item in clause:
         # connect two existing clauses
         benchmark = abs(new_clause)
         if abs(item[0]) in benchmark and abs(item[1]) in benchmark:
             # find the column # of the mathched item
-            l1 = np.where(benchmark==abs(item[0]))[0][0]
-            l2 = np.where(benchmark==abs(item[1]))[0][0]
+            l1 = np.where(benchmark == abs(item[0]))[0][0]
+            l2 = np.where(benchmark == abs(item[1]))[0][0]
             c1 = list(new_clause[l1])
             c2 = list(new_clause[l2])
             #print count, l1, l2
@@ -22,7 +23,7 @@ def preprocess(clause, num):
             # the same variables have been in the same clause
             if l1 == l2:
                 # two clauses are contradict
-                if (c1 != item).all() and (c1 != -1*item).all():
+                if (c1 != item).all() and (c1 != -1 * item).all():
                     return np.array([]), num
                 # if equal, do nothing
             else:
@@ -31,19 +32,19 @@ def preprocess(clause, num):
                     e1 = c1[0]
                 else:
                     c1.remove(-item[0])
-                    e1 = -1*c1[0]
+                    e1 = -1 * c1[0]
                 if item[1] in c2:
                     c2.remove(item[1])
                     e2 = c2[0]
                 else:
                     c2.remove(-item[1])
-                    e2 = -1*c2[0]
-                new_clause[count] = [e1,e2]
-                new_clause[l1] = new_clause[l2] = [0,0]
+                    e2 = -1 * c2[0]
+                new_clause[count] = [e1, e2]
+                new_clause[l1] = new_clause[l2] = [0, 0]
             #print "Two:", c1, c2, item, " ==> ", new_clause[count]
-        # only one of the items in the existing clauses
+            # only one of the items in the existing clauses
         elif abs(item[0]) in benchmark:
-            l1 = np.where(benchmark==abs(item[0]))[0][0]
+            l1 = np.where(benchmark == abs(item[0]))[0][0]
             c1 = list(new_clause[l1])
             #print c1
             if item[0] in c1:
@@ -51,12 +52,12 @@ def preprocess(clause, num):
                 e1 = c1[0]
             else:
                 c1.remove(-item[0])
-                e1 = -1*c1[0]
-            new_clause[count] = [e1,item[1]]
-            new_clause[l1] = [0,0]
+                e1 = -1 * c1[0]
+            new_clause[count] = [e1, item[1]]
+            new_clause[l1] = [0, 0]
             #print "First: ", c1, item, " ==> ", new_clause[count]
         elif abs(item[1]) in benchmark:
-            l2 = np.where(benchmark==abs(item[1]))[0][0]
+            l2 = np.where(benchmark == abs(item[1]))[0][0]
             c2 = list(new_clause[l2])
             #print c2
             if item[1] in c2:
@@ -64,33 +65,32 @@ def preprocess(clause, num):
                 e2 = c2[0]
             else:
                 c2.remove(-item[1])
-                e2 = -1*c2[0]
-            new_clause[count] = [item[0],e2]
-            new_clause[l2] = [0,0]
+                e2 = -1 * c2[0]
+            new_clause[count] = [item[0], e2]
+            new_clause[l2] = [0, 0]
             #print "Second: ", c2, item, " ==> ", new_clause[count]
-        # add new clauses with new items
+            # add new clauses with new items
         else:
             new_clause[count] = item
         count += 1
 
-    new_clause = np.array(filter(lambda x: x[0]!=0, list(new_clause)))
-    new_var = set(k for k in new_clause.flat if k!=0)
+    new_clause = np.array(filter(lambda x: x[0] != 0, list(new_clause)))
+    new_var = set(k for k in new_clause.flat if k != 0)
     print len(new_clause), len(new_var)
     #print new_clause
     #print new_var
-    if len(new_var) == 2*len(new_clause):
-        return np.array([[1,-1]]), 2
+    if len(new_var) == 2 * len(new_clause):
+        return np.array([[1, -1]]), 2
     else:
         freq = np.bincount(np.abs(new_clause).flat)
         benchmark = abs(new_clause)
         for i in range(len(freq)):
             if freq[i] == 1:
-                l = np.where(benchmark==i)[0][0]
-                new_clause[l] = [0,0]
-        new_clause = np.array(filter(lambda x: x[0]!=0, list(new_clause)))
-        new_var = set(k for k in new_clause.flat if k!=0)
+                l = np.where(benchmark == i)[0][0]
+                new_clause[l] = [0, 0]
+        new_clause = np.array(filter(lambda x: x[0] != 0, list(new_clause)))
+        new_var = set(k for k in new_clause.flat if k != 0)
         return new_clause, len(new_var)
-
 
 
 def sat(clause, num, new_num):
@@ -100,8 +100,8 @@ def sat(clause, num, new_num):
     for i in xrange(int(log(new_num, 2))):
         # random initial assignment
         # using length num+1 so that the index in clause can be directly mapping to
-        var = [False]+[bool(random.getrandbits(1)) for k in range(num)]
-        for j in xrange(2*new_num**2):
+        var = [False] + [bool(random.getrandbits(1)) for k in range(num)]
+        for j in xrange(2 * new_num**2):
             index = []
             for item in clause:
                 if item[0] > 0:
@@ -116,28 +116,30 @@ def sat(clause, num, new_num):
                     index.append(item)
             if len(index) == 0:
                 return True
-            pick = abs(index[random.randrange(len(index))][random.randint(0,1)])
+            pick = abs(index[random.randrange(len(index))][random.randint(0,
+                                                                          1)])
             var[pick] = not var[pick]
     # After all loops there is no satisfied condition...
     return False
 
+
 def main():
     import sys
-    assert len(sys.argv)==2, "The proper input format is: ~$ python SCRIPT.py data_file start_node"
+    assert len(
+        sys.argv) == 2, "The proper input format is: ~$ python SCRIPT.py data_file start_node"
     filename = sys.argv[1]
     with open(os.path.join(os.path.dirname(__file__), filename)) as datafile:
-            num = int(datafile.readline())
-            clause = np.zeros((num+1, 2), dtype=int)
-            count = 1
-            for row in datafile:
-                 clause[count] = [int(k) for k in finder.findall(row)]
-                 count += 1
+        num = int(datafile.readline())
+        clause = np.zeros((num + 1, 2), dtype=int)
+        count = 1
+        for row in datafile:
+            clause[count] = [int(k) for k in finder.findall(row)]
+            count += 1
 
-    clause, new_num =  preprocess(clause, num)
+    clause, new_num = preprocess(clause, num)
     print clause
     print sat(clause, num, new_num)
 
+
 if __name__ == "__main__":
     main()
-
-
