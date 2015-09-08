88f512dcc2c1a94602a1ae14018a8f9603962024
Qiong Wu <creasywuqiong@gmail.com>
2015-09-07 23:28:17 -0700

    Fix style

--- estimation_theory/blue_estimator.py
+++ estimation_theory/blue_estimator.py
@@ -3,61 +3,69 @@ from numpy import dot
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
+
 def source_localization(xn, yn, loc, noise_power):
     """input: loc -- array of locations of sensors"""
     xs = 10
     ys = 20
 
     def trigonometric(xi, yi):
-        rn = ((xn-xi)**2+(yn-yi)**2)**0.5
-        return (xn-xi)/rn, (yn-yi)/rn
-    
-    angle = np.array([list(trigonometric(i[0],i[1])) for i in loc])
-    
-    c = 3*10**8
+        rn = ((xn - xi)**2 + (yn - yi)**2)**0.5
+        return (xn - xi) / rn, (yn - yi) / rn
+
+    angle = np.array([list(trigonometric(i[0], i[1])) for i in loc])
+
+    c = 3 * 10**8
     N = len(loc)
-    A = np.zeros((N-1, N))
-    H = np.zeros((N-1, 2))
+    A = np.zeros((N - 1, N))
+    H = np.zeros((N - 1, 2))
     noise = np.random.normal(0, noise_power**0.5, N)
-    Ep = np.zeros((N-1, 1))
-    for i in range(N-1):
-        A[i,i] = -1
-        A[i,i+1] = 1
-        H[i,0] = (angle[i+1,0]-angle[i,0])/c
-        H[i,1] = (angle[i+1,1]-angle[i,1])/c
-        Ep[i] = H[i,0]*(xs-xn)+H[i,1]*(ys-yn)+noise[i+1]-noise[i]
-    C = noise_power*dot(A, A.T)
-    return dot(dot(dot(inv(dot(dot(H.T,inv(C)),H)),H.T),inv(C)),Ep).flatten()
+    Ep = np.zeros((N - 1, 1))
+    for i in range(N - 1):
+        A[i, i] = -1
+        A[i, i + 1] = 1
+        H[i, 0] = (angle[i + 1, 0] - angle[i, 0]) / c
+        H[i, 1] = (angle[i + 1, 1] - angle[i, 1]) / c
+        Ep[i] = H[i, 0] * (xs - xn) + H[i, 1] * (ys -
+                                                 yn) + noise[i + 1] - noise[i]
+    C = noise_power * dot(A, A.T)
+    return dot(
+        dot(
+            dot(
+                inv(dot(
+                    dot(H.T, inv(C)), H)), H.T), inv(C)), Ep).flatten()
 
 
 def generate_sensor(num, upper):
-    return np.random.random(size=(num, 2))*upper
+    return np.random.random(size=(num, 2)) * upper
+
 
 def main():
     xn = 10
     yn = 10
     nvar = 0.01
     high_snr = []
-    for N in range(3,31):
-        estimation = np.zeros((100,2))
+    for N in range(3, 31):
+        estimation = np.zeros((100, 2))
         for run in range(100):
             loc = generate_sensor(N, 25)
             estimation[run] = source_localization(xn, yn, loc, nvar)
-        mean = np.mean(estimation,0)
-        high_snr.append(((mean[0]-10)**2+(mean[1]-20)**2)**0.5)
+        mean = np.mean(estimation, 0)
+        high_snr.append(((mean[0] - 10)**2 + (mean[1] - 20)**2)**0.5)
     low_snr = []
     nvar = 0.1
-    for N in range(3,31):
-        estimation = np.zeros((100,2))
+    for N in range(3, 31):
+        estimation = np.zeros((100, 2))
         for run in range(100):
             loc = generate_sensor(N, 25)
             estimation[run] = source_localization(xn, yn, loc, nvar)
-        mean = np.mean(estimation,0)
-        low_snr.append(((mean[0]-10)**2+(mean[1]-20)**2)**0.5)
+        mean = np.mean(estimation, 0)
+        low_snr.append(((mean[0] - 10)**2 + (mean[1] - 20)**2)**0.5)
     plt.plot(high_snr, '-', label="SNR=20")
     plt.plot(low_snr, '-.', label="SNR=10")
     plt.legend()
     plt.show()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- estimation_theory/exponential_mle.py
+++ estimation_theory/exponential_mle.py
@@ -1,20 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def mle(x, r):
     """ Input: x -- data sequence, r -- initial guess """
     N = len(x)
+
     def guess(rk):
         # formula 7.31
-        return rk-sum((x[i]-rk**i)*i*(rk**(i-1)) for i in range(N))/sum(i*(rk**(i-2))*((i-1)*x[i]-(2*i-1)*(rk**i)) for i in range(N))
+        return rk - sum((x[i] - rk**i) * i * (rk**(i - 1))
+                        for i in range(N)) / sum(i * (rk**(i - 2)) * (
+                            (i - 1) * x[i] - (2 * i - 1) * (rk**i))
+                                                 for i in range(N))
 
     previous = r
     current = guess(r)
-    while abs(current-previous) >=0.001:
+    while abs(current - previous) >= 0.001:
         previous = current
         current = guess(current)
 
     return current
+
 
 def data_generation(N, std, r):
     """
@@ -22,26 +28,28 @@ def data_generation(N, std, r):
     Output: sequence of data according to certain distribution
     """
     mr = np.array([r**i for i in range(N)])
-    return mr+np.random.normal(0, std, N)
+    return mr + np.random.normal(0, std, N)
+
 
 def plotting(mean, var, maxlength):
     ax1 = plt.subplot(211)
     ax1.set_xticklabels(range(50, maxlength, 100))
     plt.plot(mean)
-    plt.title("Mean of MLE for "+r'$r=0.5$ and $\sigma^2=0.001$')
+    plt.title("Mean of MLE for " + r'$r=0.5$ and $\sigma^2=0.001$')
     plt.ylabel("Value of the expectation")
     ax2 = plt.subplot(212)
     ax2.set_xticklabels(range(50, maxlength, 100))
     plt.plot(var)
-    plt.title("Variance of MLE for "+r'$r=0.5$ and $\sigma^2=0.001$')
+    plt.title("Variance of MLE for " + r'$r=0.5$ and $\sigma^2=0.001$')
     plt.subplots_adjust(hspace=0.35)
     plt.ylabel("Value of the variance")
     plt.xlabel("Length of signal")
     plt.show()
 
+
 def main():
     # noise variance
-    nvar = 10**-3
+    nvar = 10** -3
     batch = 50
     r = 0.5
     maxlength = 1000
@@ -57,6 +65,6 @@ def main():
         variance.append(np.var(result))
     plotting(expect, variance, maxlength)
 
-if __name__=="__main__":
-    main()
 
+if __name__ == "__main__":
+    main()
