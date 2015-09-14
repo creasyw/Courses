8af2fccebaa69382072da355b8e343edc965b501
Qiong Wu <creasywuqiong@gmail.com>
2015-09-13 23:31:39 -0700

    Fix style

--- principles_of_computing/hw1.py
+++ principles_of_computing/hw1.py
@@ -1,19 +1,20 @@
-
 def clock_helper(total_seconds):
     """
     Helper function for a clock
     """
-    seconds_in_minute = total_seconds % 60 
+    seconds_in_minute = total_seconds % 60
+
 
 # Repeatedly append the sum of the current last three elements of lst to lst.
 def q09(n):
     sum_three = [0, 1, 2]
-    if n <=2:
+    if n <= 2:
         return sum_three[n]
     else:
-        for i in range(n-2):
+        for i in range(n - 2):
             sum_three.append(sum(sum_three[-3:]))
         return sum_three[-1]
+
 
 class BankAccount:
     def __init__(self, initial_balance):
@@ -43,6 +44,7 @@ class BankAccount:
         """Returns the total fees ever deducted from the account."""
         return self.fees
 
+
 def q10():
     my_account = BankAccount(10)
     my_account.withdraw(5)
@@ -50,7 +52,7 @@ def q10():
     my_account.withdraw(5)
     my_account.withdraw(15)
     my_account.deposit(20)
-    my_account.withdraw(5) 
+    my_account.withdraw(5)
     my_account.deposit(10)
     my_account.deposit(20)
     my_account.withdraw(15)
@@ -58,33 +60,33 @@ def q10():
     my_account.withdraw(10)
     my_account.withdraw(15)
     my_account.deposit(10)
-    my_account.withdraw(50) 
+    my_account.withdraw(50)
     my_account.deposit(30)
     my_account.withdraw(15)
     my_account.deposit(10)
-    my_account.withdraw(5) 
+    my_account.withdraw(5)
     my_account.deposit(20)
     my_account.withdraw(15)
     my_account.deposit(10)
     my_account.deposit(30)
-    my_account.withdraw(25) 
+    my_account.withdraw(25)
     my_account.withdraw(5)
     my_account.deposit(10)
     my_account.withdraw(15)
     my_account.deposit(10)
-    my_account.withdraw(10) 
+    my_account.withdraw(10)
     my_account.withdraw(15)
     my_account.deposit(10)
     my_account.deposit(30)
-    my_account.withdraw(25) 
+    my_account.withdraw(25)
     my_account.withdraw(10)
     my_account.deposit(20)
     my_account.deposit(10)
-    my_account.withdraw(5) 
+    my_account.withdraw(5)
     my_account.withdraw(15)
     my_account.deposit(10)
-    my_account.withdraw(5) 
+    my_account.withdraw(5)
     my_account.withdraw(15)
     my_account.deposit(10)
-    my_account.withdraw(5) 
+    my_account.withdraw(5)
     print my_account.get_balance(), my_account.get_fees()
