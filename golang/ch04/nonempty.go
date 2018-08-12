6bfd701a1e3b8f6af8984fadc71797fd34203536
Qiong Wu <creasywuqiong@gmail.com>
2018-08-11 19:05:22 -0700

    Add nonempty

---
+++ golang/ch04/nonempty.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func nonempty(strings []string) []string {
+	out := strings[:0]
+	for _, s := range strings {
+		if s != "" {
+			out = append(out, s)
+		}
+	}
+	return out
+}
+
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(data))
+	fmt.Printf("%q\n", data)
+}
