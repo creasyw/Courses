a86396475bfcdc81b1bb6a89b8fa502576471139
Qiong Wu <creasywuqiong@gmail.com>
2018-09-14 19:44:31 -0700

    Add clock query

---
+++ golang/ch08/clock/clock1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// this is a typical TCP server routine -
+	// Use the listener to accept the connection and then handling it
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		// When there is an error, terminate the connection
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
