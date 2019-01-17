ac8e405d46f8a71afe80eae4fe0ea1a63ec8e28b
Qiong Wu <creasywuqiong@gmail.com>
2019-01-17 08:28:08 -0800

    Add euclid's algorithm
    Signed-off-by: Qiong Wu <qiongwu@utexas.edu>

---
+++ algorithm/TAoCP/code/vol 1 - euclid algorithm.py
@@ -0,0 +1,6 @@
+# Algorithm E (Euclid’s algorithm). Given two positive integers m and n, find their greatest common
+# divisor, that is, the largest positive integer that evenly divides both m and n.
+#
+# E1. [Find remainder.] Divide m by n and let r be the remainder. (We will have 0 ≤ r < n.)
+# E2. [Is it zero?] If r = 0, the algorithm terminates; n is the answer.
+# E3. [Reduce.] Set m ← n, n ← r, and go back to step E1.
