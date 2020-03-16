1715ffd40d8ac9f7763d024ea17da403106d808f
Qiong Wu <qiong@eero.com>
2020-03-15 19:09:09 -0700

    Update code

--- learn_haskell/ch8.hs
+++ learn_haskell/ch8.hs
@@ -2,7 +2,7 @@
 data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
 
 surface :: Shape -> Float
-surface (Circle _ _ r)          = pi * r ^ 2
+surface (Circle _ _ r         ) = pi * r ^ 2
 surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
 -- A more "understandable" way to define - use "Person" to define "Person"
@@ -41,3 +41,32 @@ data Person = Person
 -- This can also be the poster boy. It is just insanely easy...
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
            deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+-- This type defines three data structures =EmptyTree=, =Node=, and =Tree=
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+-- Similar to recursive function, it starts from the edge case
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+-- This is interesting
+-- A tree is defined as (Node, Tree, Tree), which is =Node a left right= in the
+-- follow function, where =left= and =right= are two trees
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+nums = [8,6,4,1,7,3,5]
+-- it can only use =foldr= since the accumulator of the =treeInsert= is at the
+-- right side of the two input parameters
+numsTree = foldr treeInsert EmptyTree nums
