53717baa763cf181dba0b1406d56d22da4b5c55b
Qiong Wu <qiong@eero.com>
2020-03-21 15:43:50 -0700

    Code snippet of chapter 9

--- learn_haskell/ch9.hs
+++ learn_haskell/ch9.hs
@@ -1,8 +1,6 @@
 import Control.Monad as Monad
 import Data.Char as Char
-import Data.List
-import System.Directory
-import System.Environment
+import Data.List as List
 import System.IO as IO
 
 reverseInput = do
@@ -12,7 +10,7 @@ reverseInput = do
 
 reverseHyphen = do
     -- similar to `map`, `fmap` is the functor to map IO:Stream to another form
-    line <- fmap (intersperse '-' . reverse . map Char.toUpper) IO.getLine
+    line <- fmap (List.intersperse '-' . reverse . map Char.toUpper) IO.getLine
     IO.putStrLn line
 
 foreverDo = Monad.forever $ do
@@ -26,30 +24,3 @@ foreverDo = Monad.forever $ do
 foreverDo' = do
     l <- IO.getContents
     IO.putStrLn $ map Char.toUpper l
-
--- dispatch = [("add", add), ("view", view), ("remove", remove)]
--- interactiveTODO = do
---     (command : args) <- getArgs
---     let (Just action) = lookup command dispatch
---     action args
---
--- add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
---
--- view [fileName] = do
---     contents <- readFile fileName
---     let todoTasks = lines contents
---         numberedTasks =
---             zipWith (\n line -> show n ++ " - " ++ line) [1 ..] todoTasks
---     putStr $ unlines numberedTasks
---
--- remove [fileName, numberString] = do
---     handle                 <- openFile fileName ReadMode
---     (tempName, tempHandle) <- openTempFile "." "temp"
---     contents               <- hGetContents handle
---     let number       = read numberString - 1
---         todoTasks    = lines contents
---         newTodoItems = delete (todoTasks !! number) todoTasks
---     hPutStr tempHandle $ unlines newTodoItems
---     hClose handle
---     hClose tempHandle
---     renameFile tempName "new_file.txt"
