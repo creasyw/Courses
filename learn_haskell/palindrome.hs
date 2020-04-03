bac9cfe39acee19d7e7132a960a25bf1a75ec28e
Qiong Wu <qiong@eero.com>
2020-04-02 21:02:03 -0700

    Remove unused functions

--- learn_haskell/palindrome.hs
+++
@@ -1,6 +0,0 @@
-main = interact respondPalindromes
-
-respondPalindromes = unlines .
-                     map (\xs-> if isPalindrome xs then "palindrome" else "not a palindrome")
-                     . lines
-  where isPalindrome xs = xs == reverse xs
--- learn_haskell/readncaps.hs
+++
@@ -1,7 +0,0 @@
-import System.IO
-import Data.Char
-
-main = do
-  contents <- readFile "girlfriend.txt"
-  writeFile "girlfriendcaps.txt" (map toUpper contents)
-  
