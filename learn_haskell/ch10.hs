488dd8748abb39ddf7bc571b8ffc870acde49ae8
Qiong Wu <qiong@eero.com>
2021-07-12 12:00:39 -0700

    hlint

--- learn_haskell/ch10.hs
+++ learn_haskell/ch10.hs
@@ -66,7 +66,7 @@ main = do
     let threes     = groupsOf 3 (map read $ lines contents)
         roadSystem = map (\[a, b, c] -> Section a b c) threes
         path       = optimalPath roadSystem
-        pathString = concat $ map (show . fst) path
+        pathString = concatMap (show . fst) path
         pathPrice  = sum $ map snd path
     putStrLn $ "The best path to take is: " ++ pathString
     putStrLn $ "The price is: " ++ show pathPrice
