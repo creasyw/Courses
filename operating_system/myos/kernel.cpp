4d4f2bcb7254c38b1a5f32057e094223c4291e02
Qiong Wu <qiong@eero.com>
2024-09-07 21:32:02 -0700

    Add types

--- operating_system/myos/kernel.cpp
+++ operating_system/myos/kernel.cpp
@@ -1,12 +1,15 @@
+#include "types.h"
+
+
 void printf(char* str)
 {
-  unsigned short* VideoMemory = (unsigned short*) 0xb8000;
+  static uint16_t* VideoMemory = (uint16_t*) 0xb8000;
 
   for(int i=0; str[i] != '\0'; ++i)
     VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
   printf("Hello World!!!!");
 
