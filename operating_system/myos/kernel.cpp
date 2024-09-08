a9c5e25966b09de5f7f493423dcee438eb39385c
Qiong Wu <qiong@eero.com>
2024-09-07 20:57:53 -0700

    Working kernel!!!

--- operating_system/myos/kernel.cpp
+++ operating_system/myos/kernel.cpp
@@ -2,11 +2,11 @@ void printf(char* str)
 {
   unsigned short* VideoMemory = (unsigned short*) 0xb8000;
 
-  for(int i=8; str[i] != '\0'; ++i)
+  for(int i=0; str[i] != '\0'; ++i)
     VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
-void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
   printf("Hello World!!!!");
 
--- operating_system/myos/loader.s
+++ operating_system/myos/loader.s
@@ -16,6 +16,7 @@
 
 loader:
   mov $kernel_stack, %esp
+  call kernelMain
 
 
 _stop:
