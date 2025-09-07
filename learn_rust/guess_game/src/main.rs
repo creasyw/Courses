4b7462b86ff0f0400749dc0418b2511eeec305e4
Qiong Wu <qiong@eero.com>
2025-09-07 08:46:29 -0700

    Add loop and error handling

--- learn_rust/guess_game/src/main.rs
+++ learn_rust/guess_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -8,18 +8,27 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-	.expect("Failed to read line!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    let guess : u32 = guess.trim().parse().expect("Please type a number!");
-    println!("Your guess is {}.", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Your guess is {}.", guess);
 
-    match guess.cmp(&secret_number) {
-	Ordering::Less => println!("Too small!"),
-	Ordering::Greater => println!("Too big!"),
-	Ordering::Equal => println!("Win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Win!");
+                break;
+            }
+        }
     }
 }
