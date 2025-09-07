78edd6c181563c6e58691e5b97b3a46704bf6ead
Qiong Wu <qiong@eero.com>
2025-09-06 22:14:16 -0700

    Add guess game

---
+++ learn_rust/guess_game/src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+	.expect("Failed to read line!");
+
+    let guess : u32 = guess.trim().parse().expect("Please type a number!");
+    println!("Your guess is {}.", guess);
+
+    match guess.cmp(&secret_number) {
+	Ordering::Less => println!("Too small!"),
+	Ordering::Greater => println!("Too big!"),
+	Ordering::Equal => println!("Win!"),
+    }
+}
